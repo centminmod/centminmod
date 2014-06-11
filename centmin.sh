c1337f7ba64e2e981753e00945d60629a0fbb822
George Liu <eva2000@centminmod.com>
2014-06-11 15:33:15 +1000

    mailx and NGINXUPGRADESLEEP changes in centmin.sh

--- centmin.sh
+++ centmin.sh
@@ -175,7 +175,7 @@ PHP_DISABLED=n                # PHP-FPM disabled by default with chkconfig off
 MYSQLSERVICE_DISABLED=n       # MariaDB MySQL service disabled by default with chkconfig off
 
 # General Configuration
-NGINXUPGRADESLEEP='3'
+NGINXUPGRADESLEEP='6'
 NSD_INSTALL=y                # Install NSD (DNS Server)
 NSD_VERSION='3.2.17'         # NSD Version
 NTP_INSTALL=y                # Install Network time protocol daemon
@@ -852,7 +852,7 @@ then
 		echo "*************************************************"
 	
 	else
-
+        yum${CACHESKIP} -y -q install mailx
 		postfixsetup
 	fi
 
