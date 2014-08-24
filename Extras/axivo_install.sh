6345e894a880032ce29e267b57e77b870db45b8c
George Liu <eva2000@centminmod.com>
2014-08-24 20:03:18 +1000

    add standalone axivo yum repo installer

---
+++ Extras/axivo_install.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+######################################################
+# axivo yum repo manual install
+# written by George Liu (eva2000) vbtechsupport.com
+######################################################
+# variables
+#############
+DT=`date +"%d%m%y-%H%M%S"`
+
+######################################################
+
+TESTEDCENTOSVER='7.0'
+CENTOSVER=$(cat /etc/redhat-release | awk '{ print $3 }')
+
+if [ "$CENTOSVER" == 'release' ]; then
+    CENTOSVER=$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1,2)
+    if [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '7' ]]; then
+        CENTOS_SEVEN='7'
+    fi
+fi
+
+if [ "$CENTOSVER" == 'Enterprise' ]; then
+    CENTOSVER=$(cat /etc/redhat-release | awk '{ print $7 }')
+    OLS='y'
+fi
+
+# Setup Colours
+black='\E[30;40m'
+red='\E[31;40m'
+green='\E[32;40m'
+yellow='\E[33;40m'
+blue='\E[34;40m'
+magenta='\E[35;40m'
+cyan='\E[36;40m'
+white='\E[37;40m'
+
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+boldmagenta='\E[1;35;40m'
+boldcyan='\E[1;36;40m'
+boldwhite='\E[1;37;40m'
+
+Reset="tput sgr0"      #  Reset text attributes to normal
+                       #+ without clearing screen.
+
+cecho ()                     # Coloured-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+message=$1
+color=$2
+echo -e "$color$message" ; $Reset
+return
+}
+######################################################
+source "../inc/centminlogs.inc"
+source "../inc/yumskip.inc"
+source "../inc/questions.inc"
+source "../inc/downloads_centosfive.inc"
+source "../inc/downloads_centossix.inc"
+source "../inc/downloads_centosseven.inc"
+source "../inc/downloadlinks.inc"
+source "../inc/downloads.inc"
+source "../inc/yumpriorities.inc"
+source "../inc/yuminstall.inc"
+source "../inc/centoscheck.inc"
+source "../inc/axelsetup.inc"
+######################################################
+# functions
+#############
+
+if [[ "$CENTOSVER" = '6.0' || "$CENTOSVER" = '6.1' || "$CENTOSVER" = '6.2' || "$CENTOSVER" = '6.3' || "$CENTOSVER" = '6.4' || "$CENTOSVER" = '6.5' ]]; then
+{
+
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+
+if [ -s "${CENTOSSIXAXIVOFILE}" ]; then
+  echo "${CENTOSSIXAXIVOFILE} [found]"
+  else
+  echo "Error: ${CENTOSSIXAXIVOFILE} not found !!! Download now......"
+  wget ${WGETOPT} ${CENTOSSIXAXIVO} --tries=3
+  # wget ${WGETOPT} ${CENTOSSIXAXIVOLOCAL} --tries=3
+ERROR=$?
+	if [[ "$ERROR" != '0' ]]; then
+	cecho "Error: ${CENTOSSIXAXIVOFILE} download failed." $boldgreen
+FAILAXIVO='y'
+checklogdetails
+	exit #$ERROR
+else 
+	cecho "Download done." $boldyellow
+	rpm -ivh --nosignature ${CENTOSSIXAXIVOFILE}
+	yum${CACHESKIP} -q list ccache 2>&1 >/dev/null
+	ERR=$?
+	CCAXIVOCHECK="$ERR"
+		if [ "$CCAXIVOCHECK" == '0' ]; then
+			yumprioraxivo
+			echo ""
+		else
+			cecho "Error: problem with ${CENTOSSIXAXIVOFILE} install." $boldgreen
+			exit $ERR
+		fi
+	fi
+
+fi
+
+fi
+
+} 2>&1 | tee ${CENTMINLOGDIR}/axivo_install_${DT}_centos6.log
+fi
+
+if [[ "$CENTOSVER" = '7.0' ]]; then
+{
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+
+if [ -s "${CENTOSSEVENAXIVOFILE}" ]; then
+  echo "${CENTOSSEVENAXIVOFILE} [found]"
+  else
+  echo "Error: ${CENTOSSEVENAXIVOFILE} not found !!! Download now......"
+  wget ${WGETOPT} ${CENTOSSEVENAXIVO} --tries=3
+  # wget ${WGETOPT} ${CENTOSSEVENAXIVOLOCAL} --tries=3
+ERROR=$?
+  if [[ "$ERROR" != '0' ]]; then
+  cecho "Error: ${CENTOSSEVENAXIVOFILE} download failed." $boldgreen
+FAILAXIVO='y'
+checklogdetails
+  exit #$ERROR
+else 
+  cecho "Download done." $boldyellow
+  rpm -ivh --nosignature ${CENTOSSEVENAXIVOFILE}
+  yum${CACHESKIP} -q list ccache 2>&1 >/dev/null
+  ERR=$?
+  CCAXIVOCHECK="$ERR"
+    if [ "$CCAXIVOCHECK" == '0' ]; then
+      yumprioraxivo
+      echo ""
+    else
+      cecho "Error: problem with ${CENTOSSEVENAXIVOFILE} install." $boldgreen
+      exit $ERR
+    fi
+  fi
+
+fi
+
+fi
+
+} 2>&1 | tee ${CENTMINLOGDIR}/axivo_install_${DT}_centos7.log
+fi
+
+
+######################################################
