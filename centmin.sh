8752f165615262d561b7055b2b1b0aab87ae0f3f
George Liu <eva2000@centminmod.com>
2014-12-20 00:59:31 +1000

    update to PHP 5.4.36 default for Centmin Mod .08 beta

--- centmin.sh
+++ centmin.sh
@@ -324,7 +324,7 @@ TWEMPERF_VER='0.1.1'
 
 FFMPEGVER='0.6.0'
 SUHOSINVER='0.9.36'
-PHP_VERSION='5.4.35'          # Use this version of PHP
+PHP_VERSION='5.4.36'          # Use this version of PHP
 PHP_MIRRORURL='http://php.net'
 PHPUPGRADE_MIRRORURL='http://php.net'
 XCACHE_VERSION='3.2.0'       # Use this version of Xcache
