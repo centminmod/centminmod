359b6308d20aff2cdb1b588814a29ff4c98235e5
George Liu <eva2000@centminmod.com>
2015-03-25 03:45:29 +1000

    update Nginx 1.7.11 default

--- centmin.sh
+++ centmin.sh
@@ -296,7 +296,7 @@ MYSQL_INSTALL=n              # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL=n           # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.7.10'        # Use this version of Nginx
+NGINX_VERSION='1.7.11'        # Use this version of Nginx
 NGINXBACKUP='y'
 NGINXDIR='/usr/local/nginx'
 NGINXCONFDIR="${NGINXDIR}/conf"
