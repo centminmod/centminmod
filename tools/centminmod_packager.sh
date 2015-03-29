c005e8c9755931008fd7db430cef00308dd4c398
George Liu <eva2000@centminmod.com>
2015-03-30 04:33:11 +1000

    add tools/centminmod_packager.sh tool

---
+++ tools/centminmod_packager.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+################################################
+# package up the Centmin Mod required downloads
+# from /svr-setup into a zip package you can
+# reuse locally for new installs without having
+# to redownload the entire info
+################################################
+DT=`date +"%d%m%y-%H%M%S"`
+PKGBASE='/home'
+PKGDIR="${PKGBASE}/centminmod_pkg"
+PKGNAME='centminmod_pkg'
+
+################################################
+mkdir -p $PKGDIR
+
+for f in "$(ls /svr-setup-package)"
+ do 
+	if [[ "$(stat --printf='%F' $f)" != 'directory' ]]; then 
+		echo "copying $f to $PKGDIR"
+		\cp -f $f $PKGDIR
+	fi
+done
+
+echo
+echo "copy ngx_pagespeed + psol"
+cd /svr-setup-package
+\cp -a $(ls -t | grep ngx_pagespeed-release | head -n1) $PKGDIR
+
+echo
+du -sh $PKGDIR
+ls -lah $PKGDIR
+
+echo
+echo "zip up package directory $PKGDIR"
+cd $PKGBASE
+time zip -4 -q -r ${PKGNAME}-$DT $PKGDIR
+
+echo
+ls -lah $PKGBASE | grep $PKGNAME
+
+echo
+echo "packaging completed"
+echo "saved package to ${PKGDIR}/$PKGNAME"
+exit
