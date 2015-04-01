f9e3545f4da486a310beac32ef64a4ad2d080916
George Liu <eva2000@centminmod.com>
2015-04-01 18:56:04 +1000

    update ImagicK PHP Extension from 3.2.0 RC1 to 3.3.0 RC1

--- centmin.sh
+++ centmin.sh
@@ -316,7 +316,7 @@ PCRE_SOURCEINSTALL=n
 PCRE_VERSION='8.36'          # NO longer used/ignored
 
 # PHP and Cache/Acceleration
-IMAGICKPHP_VER='3.2.0RC1'   # PHP extension for imagick
+IMAGICKPHP_VER='3.3.0RC1'   # PHP extension for imagick
 MEMCACHED_INSTALL=y          # Install Memcached
 LIBEVENT_VERSION='2.0.22'    # Use this version of Libevent
 MEMCACHED_VERSION='1.4.22'    # Use this version of Memcached server
