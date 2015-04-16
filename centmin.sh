6fe45c19a74feaa9630a48913120f3afbd2151f0
George Liu <eva2000@centminmod.com>
2015-04-17 07:51:11 +1000

    PHP 5.4.40 default for .08 beta

--- centmin.sh
+++ centmin.sh
@@ -330,7 +330,7 @@ REDISPHP_VER='2.2.7'        # redis PHP version
 
 FFMPEGVER='0.6.0'
 SUHOSINVER='0.9.37.1'
-PHP_VERSION='5.4.39'          # Use this version of PHP
+PHP_VERSION='5.4.40'          # Use this version of PHP
 PHP_MIRRORURL='http://php.net'
 PHPUPGRADE_MIRRORURL='http://php.net'
 XCACHE_VERSION='3.2.0'       # Use this version of Xcache
