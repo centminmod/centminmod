6744e36ffe1c384f506adf7056d56df8b74cfbc0
George Liu <eva2000@centminmod.com>
2015-05-06 00:32:54 +1000

    add persistent custom centmin.sh options file support at inc/custom_config.inc supports 2 locations for custom_config.inc 1). inc/custom_config.inc where centmin.sh base directory resides 2). at ${CONFIGSCANBASE}/custom_config.inc which defaults to /etc/centminmod/custom_config.inc to override centmin.sh settings and allow them to persist on centmin mod code updates, create a custom_config.inc at one of the 2 locations mentioned above and add the centmin.sh option to the custom_config.inc file i.e.
    PHP_VERSION='5.6.8'
    ZOPCACHEDFT='y'

--- centmin.sh
+++ centmin.sh
@@ -513,6 +513,15 @@ ASKCMD="read $KEYPRESS_PARAM "
 
 CUR_DIR=`pwd` # Get current directory.
 CM_INSTALLDIR=$CUR_DIR
+
+if [ -f "${CM_INSTALLDIR}/inc/custom_config.inc" ]; then
+    source "inc/custom_config.inc"
+fi
+
+if [ -f "${CONFIGSCANBASE}/custom_config.inc" ]; then
+    # default is at /etc/centminmod/custom_config.inc
+    source "${CONFIGSCANBASE}/custom_config.inc"
+fi
 ###############################################################
 # FUNCTIONS
 
