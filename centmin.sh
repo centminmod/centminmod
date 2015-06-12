9bd9c8d1322133bfd7af12b2dd484f31e264185d
George Liu <eva2000@centminmod.com>
2015-06-12 14:53:21 +1000

    PHP 5.4.42 default for .08 beta 03

--- centmin.sh
+++ centmin.sh
@@ -381,7 +381,7 @@ MONGODB_SASL='n'            # SASL not working yet leave = n
 
 FFMPEGVER='0.6.0'
 SUHOSINVER='0.9.37.1'
-PHP_VERSION='5.4.41'          # Use this version of PHP
+PHP_VERSION='5.4.42'          # Use this version of PHP
 PHP_MIRRORURL='http://php.net'
 PHPUPGRADE_MIRRORURL='http://php.net'
 XCACHE_VERSION='3.2.0'       # Use this version of Xcache
