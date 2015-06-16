27c4ca2472addc04b97a480a5653dcb5b397ade0
George Liu <eva2000@centminmod.com>
2015-06-16 11:58:44 +1000

    update centmin.sh & inc/nginx_configure.inc with NGINX_STREAM variable

--- centmin.sh
+++ centmin.sh
@@ -254,6 +254,7 @@ STRIPNGINX='y'               # set 'y' to strip nginx binary to reduce size
 NGINX_INSTALL=y              # Install Nginx (Webserver)
 NGINX_GEOIP=y                # Nginx GEOIP module install
 NGINX_SPDY=y                 # Nginx SPDY support
+NGINX_STREAM=y               # http://nginx.org/en/docs/stream/ngx_stream_core_module.html
 NGINX_RTMP=n                 # Nginx RTMP Module support https://github.com/arut/nginx-rtmp-module
 NGINX_MP4=n                  # Nginx MP4 Module http://nginx.org/en/docs/http/ngx_http_mp4_module.html
 NGINX_VHOSTSTATS=y           # https://github.com/vozlt/nginx-module-vts
