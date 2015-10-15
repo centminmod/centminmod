658198aeb62a482c7b182049418bf0db719eab40
George Liu <eva2000@centminmod.com>
2015-10-15 16:15:10 +1000

    update addons/devtoolset-3.sh
    setup colors

--- addons/devtoolset-3.sh
+++ addons/devtoolset-3.sh
@@ -16,6 +16,38 @@ DIR_TMP='/svr-setup'
 
 
 ########################################################################
+# Setup Colours
+black='\E[30;40m'
+red='\E[31;40m'
+green='\E[32;40m'
+yellow='\E[33;40m'
+blue='\E[34;40m'
+magenta='\E[35;40m'
+cyan='\E[36;40m'
+white='\E[37;40m'
+
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+boldmagenta='\E[1;35;40m'
+boldcyan='\E[1;36;40m'
+boldwhite='\E[1;37;40m'
+
+Reset="tput sgr0"      #  Reset text attributes to normal
+                       #+ without clearing screen.
+
+cecho ()                     # Coloured-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+message=$1
+color=$2
+echo -e "$color$message" ; $Reset
+return
+}
+########################################################################
 # functions
 #############
 CENTOSVER=$(awk '{ print $3 }' /etc/redhat-release)
