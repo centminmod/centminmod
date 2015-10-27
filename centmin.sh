40364b3917bb26d767e6a4746785f1b8ac0f7177
George Liu <eva2000@centminmod.com>
2015-10-28 00:33:15 +1000

    nginx 1.9.6 default

--- centmin.sh
+++ centmin.sh
@@ -391,7 +391,7 @@ MYSQL_INSTALL=n              # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL=n           # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.9.5'        # Use this version of Nginx
+NGINX_VERSION='1.9.6'        # Use this version of Nginx
 NGINX_VHOSTSSL='y'           # enable centmin.sh menu 2 prompt to create self signed SSL vhost 2nd vhost conf
 NGINXBACKUP='y'
 NGINXDIR='/usr/local/nginx'
