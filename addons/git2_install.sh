250634fcf417d0377747584cea63700f0044a25a
George Liu <eva2000@centminmod.com>
2015-11-27 21:44:19 +1000

    add addons/git2_install.sh
    IUS Community YUM Repo sourced Git 2.6.x update addon to replace system Git 1.8 as per https://community.centminmod.com/posts/20898/

---
+++ addons/git2_install.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+###########################################################
+# installs IUS Community YUM Repository
+# https://iuscommunity.org/pages/Repos.html
+# for access to git 2.6.x branch
+# https://community.centminmod.com/posts/20898/
+###########################################################
+DT=`date +"%d%m%y-%H%M%S"`
+CENTMINLOGDIR='/root/centminlogs'
+DIR_TMP='/svr-setup'
+
+###########################################################
+CENTOSVER=$(awk '{ print $3 }' /etc/redhat-release)
+
+if [ "$CENTOSVER" == 'release' ]; then
+    CENTOSVER=$(awk '{ print $4 }' /etc/redhat-release | cut -d . -f1,2)
+    if [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '7' ]]; then
+        CENTOS_SEVEN='7'
+    fi
+fi
+
+if [[ "$(cat /etc/redhat-release | awk '{ print $3 }' | cut -d . -f1)" = '6' ]]; then
+    CENTOS_SIX='6'
+fi
+
+if [ "$CENTOSVER" == 'Enterprise' ]; then
+    CENTOSVER=$(cat /etc/redhat-release | awk '{ print $7 }')
+    OLS='y'
+fi
+###########################################################
+# Setup Colours
+black='\E[30;40m'
+red='\E[31;40m'
+green='\E[32;40m'
+yellow='\E[33;40m'
+blue='\E[34;40m'
+magenta='\E[35;40m'
+cyan='\E[36;40m'
+white='\E[37;40m'
+
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+boldmagenta='\E[1;35;40m'
+boldcyan='\E[1;36;40m'
+boldwhite='\E[1;37;40m'
+
+Reset="tput sgr0"      #  Reset text attributes to normal
+                       #+ without clearing screen.
+
+cecho ()                     # Coloured-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+message=$1
+color=$2
+echo -e "$color$message" ; $Reset
+return
+}
+
+###########################################################
+starttime=$(date +%s.%N)
+{
+
+if [[ "$CENTOS_SIX" = '6' ]]; then
+    if [ ! -f /etc/yum.repos.d/ius.repo ]; then
+        yum -y install https://centos6.iuscommunity.org/ius-release.rpm
+    fi
+elif [[ "$CENTOS_SEVEN" = '7' ]]; then
+    if [ ! -f /etc/yum.repos.d/ius.repo ]; then
+        yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+    fi
+fi
+
+# disable by default the ius.repo
+sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/ius.repo
+
+if [ -f /etc/yum.repos.d/ius.repo ]; then
+cp -p /etc/yum.repos.d/ius.repo /etc/yum.repos.d/ius.OLD
+if [ -n "$(grep ^priority /etc/yum.repos.d/ius.repo)" ]
+        then
+                #echo priorities already set for ius.repo
+PRIOREXISTS=1
+        else
+                echo "setting yum priorities for ius.repo"
+                ex -s /etc/yum.repos.d/ius.repo << EOF
+:/\[ius/ , /gpgkey/
+:a
+priority=98
+.
+:w
+:/\[ius-debuginfo/ , /gpgkey/
+:a
+priority=98
+.
+:w
+:/\[ius-source/ , /gpgkey/
+:a
+priority=98
+.
+:w
+:q
+EOF
+
+cecho "*************************************************" $boldgreen
+cecho "Fixing ius.repo YUM Priorities" $boldgreen
+cecho "*************************************************" $boldgreen
+echo "cat /etc/yum.repos.d/ius.repo"
+cat /etc/yum.repos.d/ius.repo
+echo ""
+fi
+fi # repo file check
+
+cecho "*************************************************" $boldgreen
+cecho "Installing Git 2.6.*" $boldgreen
+cecho "*************************************************" $boldgreen
+
+# install Git 2.6 package to replace Git 1.8 package
+yum -y install yum-plugin-replace --enablerepo=ius
+yum -y replace git --replace-with git2u --enablerepo=ius
+
+echo
+git --version
+
+} 2>&1 | tee ${CENTMINLOGDIR}/git2u-install_${DT}.log
+
+endtime=$(date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> ${CENTMINLOGDIR}/git2u-install_${DT}.log
+echo "Git 2.6.* Install Time: $INSTALLTIME seconds" >> ${CENTMINLOGDIR}/git2u-install_${DT}.log
