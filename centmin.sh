1cc48973d5e55186506f8ef377852acc49a87948
George Liu <eva2000@centminmod.com>
2016-01-28 12:52:00 +1000

    update addons/customcurl.sh addon 7.47
    Update customcurl.sh addon to 7.47 http://curl.haxx.se/changes.html

--- centmin.sh
+++ centmin.sh
@@ -491,7 +491,7 @@ ATRPMSREPO_DISABLE=y
 # use at own risk as it can break the system
 # info at http://mirror.city-fan.org/ftp/contrib/sysutils/Mirroring/
 CUSTOM_CURLRPM=n
-CUSTOM_CURLRPMVER='7.46.0-2.0'       # custom curl/libcurl version
+CUSTOM_CURLRPMVER='7.47.0-1.0'       # custom curl/libcurl version
 CUSTOM_CURLLIBSSHVER='1.6.0-3.0'     # libssh2 version
 CUSTOM_CURLRPMCARESVER='1.10.0-5.0'  # c-ares version
 CUSTOM_CURLRPMSYSURL='http://mirror.city-fan.org/ftp/contrib/sysutils/Mirroring'
