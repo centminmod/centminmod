db3ec3807a4e4316b6f6f56bf895ca36144ff3e9
George Liu <eva2000@centminmod.com>
2016-02-10 07:10:26 +1000

    revert to nginx 1.9.10 due to 1.9.11 nginx module conflicts
    https://community.centminmod.com/posts/26066/

--- centmin.sh
+++ centmin.sh
@@ -415,7 +415,7 @@ MYSQL_INSTALL=n              # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL=n           # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.9.11'        # Use this version of Nginx
+NGINX_VERSION='1.9.10'        # Use this version of Nginx
 NGINX_VHOSTSSL='y'           # enable centmin.sh menu 2 prompt to create self signed SSL vhost 2nd vhost conf
 NGINXBACKUP='y'
 NGINXDIR='/usr/local/nginx'
