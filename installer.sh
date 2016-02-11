535e952eda93c4d807ecaf414815ddbbdb418340
George Liu <eva2000@centminmod.com>
2016-02-12 07:23:55 +1000

    tidy up code for installer.sh and inc/nginx_upgrade.inc

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@
 # curl -sL https://gist.github.com/centminmod/dbe765784e03bc4b0d40/raw/installer.sh | bash
 #######################################################
 export PATH="/usr/lib64/ccache:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
-DT=`date +"%d%m%y-%H%M%S"`
+DT=$(date +"%d%m%y-%H%M%S")
 branchname=123.09beta01
 DOWNLOAD="${branchname}.zip"
 
@@ -186,13 +186,13 @@ echo "--------------------------------------------------------------------------
   tail -1 /root/centminlogs/firstyum_installtime_*.log
   tail -1 /root/centminlogs/centminmod_yumtimes_*.log
   DTIME=$(tail -1 /root/centminlogs/centminmod_downloadtimes_*.log)
-  DTIME_SEC=$(echo $DTIME |awk '{print $7}')
+  DTIME_SEC=$(echo "$DTIME" |awk '{print $7}')
   NTIME=$(tail -1 /root/centminlogs/centminmod_ngxinstalltime_*.log)
-  NTIME_SEC=$(echo $NTIME |awk '{print $7}')
+  NTIME_SEC=$(echo "$NTIME" |awk '{print $7}')
   PTIME=$(tail -1 /root/centminlogs/centminmod_phpinstalltime_*.log)
-  PTIME_SEC=$(echo $PTIME |awk '{print $7}')
+  PTIME_SEC=$(echo "$PTIME" |awk '{print $7}')
   CMTIME=$(tail -1 /root/centminlogs/*_install.log)
-  CMTIME_SEC=$(echo $CMTIME |awk '{print $6}')
+  CMTIME_SEC=$(echo "$CMTIME" |awk '{print $6}')
   CMTIME_SEC=$(printf "%0.4f\n" $CMTIME_SEC)
   CURLT=$(awk '{print $6}' /root/centminlogs/firstyum_installtime_*.log | tail -1)
   CT=$(awk '{print $6}' /root/centminlogs/*_install.log | tail -1)
