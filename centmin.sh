318f61f0c8b88a4772cc56cc3f19264f7277c8e6
George Liu <eva2000@centminmod.com>
2016-06-12 02:06:33 +1000

    update imagick 3.4.3RC1 php extension for PHP 7.1 support

--- centmin.sh
+++ centmin.sh
@@ -485,7 +485,7 @@ PCRE_SOURCEINSTALL='n'
 PCRE_VERSION='8.38'          # NO longer used/ignored
 
 # PHP and Cache/Acceleration
-IMAGICKPHP_VER='3.4.2'   # PHP extension for imagick
+IMAGICKPHP_VER='3.4.3RC1'   # PHP extension for imagick
 MAILPARSEPHP_VER='2.1.6'    # https://pecl.php.net/package/mailparse
 MAILPARSEPHP_COMPATVER='3.0.1' # For PHP 7
 MEMCACHED_INSTALL='y'         # Install Memcached
