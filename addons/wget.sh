07b99f32e2e655917fbea32c6560b3de3fa96f8f
George Liu <eva2000@centminmod.com>
2016-07-08 01:19:11 +1000

    add addons/wget.sh allow newer version of wget on CentOS for 123.09beta01
    addons/wget.sh allows source compilation of a newer version of wget than the YUM repo available CentOS wget versions. The source install via addons/wget.sh installs newer wget to /usr/local/bin/wget and sets up wget command as an alias pointing to that binary path so as to not interfere with CentOS YUM installed wget version.

---
+++ addons/wget.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+###########################################################
+# wget source installer to /usr/local/bin/wget path for
+# centminmod.com LEMP stacks
+# installs newer wget version than available via centos RPM
+# repos but does not interfere with YUM installed wget as it
+# is just an alias wget command setup
+###########################################################
+DT=$(date +"%d%m%y-%H%M%S")
+CENTMINLOGDIR='/root/centminlogs'
+DIR_TMP='/svr-setup'
+
+ALTPCRE_VERSION='8.39'
+ALTPCRELINKFILE="pcre-${ALTPCRE_VERSION}.tar.gz"
+ALTPCRELINK="https://centminmod.com/centminmodparts/pcre/${ALTPCRELINKFILE}"
+
+WGET_VERSION='1.18'
+WGET_FILENAME="wget-${WGET_VERSION}.tar.gz"
+WGET_LINK="http://ftpmirror.gnu.org/wget/${WGET_FILENAME}"
+###########################################################
+CENTOSVER=$(awk '{ print $3 }' /etc/redhat-release)
+
+if [ ! -d "$CENTMINLOGDIR" ]; then
+  mkdir -p "$CENTMINLOGDIR"
+fi
+
+if [ "$CENTOSVER" == 'release' ]; then
+    CENTOSVER=$(awk '{ print $4 }' /etc/redhat-release | cut -d . -f1,2)
+    if [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '7' ]]; then
+        CENTOS_SEVEN='7'
+    fi
+fi
+
+if [[ "$(cat /etc/redhat-release | awk '{ print $3 }' | cut -d . -f1)" = '6' ]]; then
+    CENTOS_SIX='6'
+fi
+
+if [ "$CENTOSVER" == 'Enterprise' ]; then
+    CENTOSVER=$(cat /etc/redhat-release | awk '{ print $7 }')
+    OLS='y'
+fi
+
+if [ -f /proc/user_beancounters ]; then
+    # CPUS='1'
+    # MAKETHREADS=" -j$CPUS"
+    # speed up make
+    CPUS=`grep "processor" /proc/cpuinfo |wc -l`
+    CPUS=$(echo $CPUS+1 | bc)
+    MAKETHREADS=" -j$CPUS"
+else
+    # speed up make
+    CPUS=`grep "processor" /proc/cpuinfo |wc -l`
+    CPUS=$(echo $CPUS+1 | bc)
+    MAKETHREADS=" -j$CPUS"
+fi
+
+###########################################################
+# Setup Colours
+black='\E[30;40m'
+red='\E[31;40m'
+green='\E[32;40m'
+yellow='\E[33;40m'
+blue='\E[34;40m'
+magenta='\E[35;40m'
+cyan='\E[36;40m'
+white='\E[37;40m'
+
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+boldmagenta='\E[1;35;40m'
+boldcyan='\E[1;36;40m'
+boldwhite='\E[1;37;40m'
+
+Reset="tput sgr0"      #  Reset text attributes to normal
+                       #+ without clearing screen.
+
+cecho ()                     # Coloured-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+message=$1
+color=$2
+echo -e "$color$message" ; $Reset
+return
+}
+
+###########################################################
+
+source_pcreinstall() {
+  cd "$DIR_TMP"
+  cecho "Download $ALTPCRELINKFILE ..." $boldyellow
+  if [ -s "$ALTPCRELINKFILE" ]; then
+    cecho "$ALTPCRELINKFILE Archive found, skipping download..." $boldgreen
+  else
+    wget -c --progress=bar "$ALTPCRELINK" --tries=3 
+    ERROR=$?
+    if [[ "$ERROR" != '0' ]]; then
+      cecho "Error: $ALTPCRELINKFILE download failed." $boldgreen
+      exit #$ERROR
+    else 
+      cecho "Download done." $boldyellow
+    fi
+  fi
+  
+  tar xzf "$ALTPCRELINKFILE"
+  ERROR=$?
+  if [[ "$ERROR" != '0' ]]; then
+    cecho "Error: $ALTPCRELINKFILE extraction failed." $boldgreen
+    exit #$ERROR
+  else 
+    cecho "$ALTPCRELINKFILE valid file." $boldyellow
+    echo ""
+  fi
+  cd "pcre-${ALTPCRE_VERSION}"
+  ./configure --enable-pcre16 --enable-pcre32 --enable-pcregrep-libz --enable-pcregrep-libbz2 --enable-pcretest-libreadline
+  make${MAKETHREADS}
+  make install
+  /usr/local/bin/pcre-config --version
+}
+
+source_wgetinstall() {
+  cd "$DIR_TMP"
+  cecho "Download $WGET_FILENAME ..." $boldyellow
+  if [ -s "$WGET_FILENAME" ]; then
+    cecho "$WGET_FILENAME Archive found, skipping download..." $boldgreen
+  else
+    wget -c --progress=bar "$WGET_LINK" --tries=3 
+    ERROR=$?
+    if [[ "$ERROR" != '0' ]]; then
+      cecho "Error: $WGET_FILENAME download failed." $boldgreen
+      exit #$ERROR
+    else 
+      cecho "Download done." $boldyellow
+    fi
+  fi
+  
+  tar xzf "$WGET_FILENAME"
+  ERROR=$?
+  if [[ "$ERROR" != '0' ]]; then
+    cecho "Error: $WGET_FILENAME extraction failed." $boldgreen
+    exit #$ERROR
+  else 
+    cecho "$WGET_FILENAME valid file." $boldyellow
+    echo ""
+  fi
+  cd "wget-${WGET_VERSION}"
+  make clean
+  export CFLAGS="-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic"
+  ./configure --with-ssl=openssl PCRE_CFLAGS="-I /usr/local/include" PCRE_LIBS="-L /usr/local/lib -lpcre"
+  make${MAKETHREADS}
+  make install
+  if [[ ! "$(grep '^alias wget' /root/.bashrc)" ]]; then
+    echo "alias wget='/usr/local/bin/wget'" >> /root/.bashrc
+  fi
+  . /root/.bashrc
+ 
+  cecho "--------------------------------------------------------" $boldgreen
+  cecho "wget -V" $boldyellow
+  wget -V
+  cecho "--------------------------------------------------------" $boldgreen
+  cecho "wget ${WGET_VERSION} installed at /usr/local/bin/gwet" $boldyellow
+  cecho "--------------------------------------------------------" $boldgreen
+  echo
+}
+
+###########################################################################
+case $1 in
+  install)
+starttime=$(date +%s.%N)
+{
+  source_pcreinstall
+  source_wgetinstall
+} 2>&1 | tee "${CENTMINLOGDIR}/wget_source_install_${DT}.log"
+
+endtime=$(date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> "${CENTMINLOGDIR}/wget_source_install_${DT}.log"
+echo "Total wget Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/wget_source_install_${DT}.log"
+tail -1 "${CENTMINLOGDIR}/wget_source_install_${DT}.log"
+  ;;
+  *)
+    echo "$0 install"
+  ;;
+esac
+exit
