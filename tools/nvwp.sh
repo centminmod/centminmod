03b6a062883f2d4f2a6f7895a54b71e80f191ab2
George Liu <eva2000@centminmod.com>
2016-07-10 17:58:01 +1000

    update inc/wpsetup.inc & tools/nvwp.sh 123.09beta01
    add woocommerce_items_in_cart cookie to cache exclusions

--- tools/nvwp.sh
+++ tools/nvwp.sh
@@ -883,7 +883,7 @@ if (\$query_string != "") { set \$cache_uri 'null cache'; }
 
 if (\$request_uri ~* "/(\?add-to-cart=|cart|my-account|checkout|addons|wp-admin/.*|xmlrpc\.php|wp-.*\.php|index\.php|feed/|sitemap(_index)?\.xml|[a-z0-9_-]+-sitemap([0-9]+)?\.xml)") { set \$cache_uri 'null cache'; }
 
-if (\$http_cookie ~* "comment_author|wordpress_[a-f0-9]+|wp-postpass|wordpress_logged_in") { set \$cache_uri 'null cache'; }
+if (\$http_cookie ~* "comment_author|wordpress_[a-f0-9]+|wp-postpass|wordpress_logged_in|woocommerce_items_in_cart") { set \$cache_uri 'null cache'; }
 EFF
 
 ######### Wordpress Manual Install no WP-CLI ######################
