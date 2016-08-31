7f420193d254c92c05441e5a4c6785be101de417
George Liu <eva2000@centminmod.com>
2016-09-01 09:30:42 +1000

    update for pcre UTF8 support

--- installer56.sh
+++ installer56.sh
@@ -184,7 +184,7 @@ source_pcreinstall() {
     echo ""
   fi
   cd "pcre-${ALTPCRE_VERSION}"
-  ./configure --enable-pcre16 --enable-pcre32 --enable-pcregrep-libz --enable-pcregrep-libbz2 --enable-pcretest-libreadline
+  ./configure --enable-utf8 --enable-unicode-properties --enable-pcre16 --enable-pcre32 --enable-pcregrep-libz --enable-pcregrep-libbz2 --enable-pcretest-libreadline
   make${MAKETHREADS}
   make install
   /usr/local/bin/pcre-config --version
