f30599a4505c5d1c70087bb786faecd55163c8bc
George Liu <eva2000@centminmod.com>
2016-10-01 11:32:59 +1000

    prepare for CentOS 6 may need GCC higher version for PGO PHP7 in 123.09beta01

--- centmin.sh
+++ centmin.sh
@@ -286,6 +286,7 @@ CLANG_APC='n'                 # APC Cache
 CLANG_MEMCACHED='n'           # Memcached menu option 10 routine
 GCCINTEL_PHP='y'              # enable PHP-FPM GCC compiler with Intel cpu optimizations
 PHP_PGO='n'                   # Profile Guided Optimization https://software.intel.com/en-us/blogs/2015/10/09/pgo-let-it-go-php
+PHP_PGO_CENTOSSIX='n'         # CentOS 6 may need GCC >4.4.7 fpr PGO so use devtoolset-3 GCC 4.9
 
 # When set to =y, will disable those listed installed services 
 # by default. The service is still installed but disabled 
