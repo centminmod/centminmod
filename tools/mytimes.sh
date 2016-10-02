4b19b54d14b4e830ef1d25828e014ae261f30b62
George Liu <eva2000@centminmod.com>
2016-10-03 00:54:41 +1000

    extend mytimes command shortcut timezones displayed in 123.09beta01
    [CODEB]
    mytimes
    Sun Oct  2 14:53:36 UTC 2016    [UTC]
    Mon Oct  3 00:53:36 AEST 2016   [Australia/Brisbane]
    Sun Oct  2 07:53:36 PDT 2016    [America/Los_Angeles]
    Sun Oct  2 09:53:36 CDT 2016    [America/Chicago]
    Sun Oct  2 10:53:36 EDT 2016    [America/New_York]
    Sun Oct  2 10:53:36 EDT 2016    [America/Montreal]
    Sun Oct  2 15:53:36 BST 2016    [Europe/London]
    Sun Oct  2 16:53:36 CEST 2016   [Europe/Berlin]
    Sun Oct  2 21:53:36 ICT 2016    [Asia/Bangkok]
    Sun Oct  2 21:53:36 ICT 2016    [Asia/Ho_Chi_Minh]
    Sun Oct  2 21:53:36 WIB 2016    [Asia/Jakarta]
    Sun Oct  2 22:53:36 MYT 2016    [Asia/Kuala_Lumpur]
    Sun Oct  2 22:53:36 SGT 2016    [Asia/Singapore]
    [/CODEB

--- tools/mytimes.sh
+++ tools/mytimes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TIMEZONES='UTC Australia/Brisbane America/Los_Angeles America/Chicago America/New_York Europe/London'
+TIMEZONES='UTC Australia/Brisbane America/Los_Angeles America/Chicago America/New_York America/Montreal Europe/London Europe/Berlin Asia/Bangkok Asia/Ho_Chi_Minh Asia/Jakarta Asia/Kuala_Lumpur Asia/Singapore'
 
 for t in $TIMEZONES; do
     echo -e "$(TZ=$t date)\t[$t]";
