87cf78cd02806b1fb1c2970b1a7e08daec46c470
George Liu <eva2000@centminmod.com>
2016-10-18 02:52:38 +1000

    2nd fix log persistent config file contents each centmin.sh run in 123.09beta01
    for troubleshooting issues

--- centmin.sh
+++ centmin.sh
@@ -828,7 +828,7 @@ CM_INSTALLDIR=$CUR_DIR
 if [ -f "${CM_INSTALLDIR}/inc/custom_config.inc" ]; then
     source "inc/custom_config.inc"
     if [ -d "${CENTMINLOGDIR}" ]; then
-        cat "${CONFIGSCANBASE}/custom_config.inc" > "${CENTMINLOGDIR}/etc-centminmod-custom-config-settings_${DT}.log"
+        cat "inc/custom_config.inc" > "${CENTMINLOGDIR}/inc-custom-config-settings_${DT}.log"
     fi
 fi
 
@@ -836,7 +836,7 @@ if [ -f "${CONFIGSCANBASE}/custom_config.inc" ]; then
     # default is at /etc/centminmod/custom_config.inc
     source "${CONFIGSCANBASE}/custom_config.inc"
     if [ -d "${CENTMINLOGDIR}" ]; then
-        cat "${CONFIGSCANBASE}/custom_config.inc" > "${CENTMINLOGDIR}/config-scanbase-custom-config-settings_${DT}.log"
+        cat "${CONFIGSCANBASE}/custom_config.inc" > "${CENTMINLOGDIR}/etc-centminmod-custom-config-settings_${DT}.log"
     fi
 fi
 
