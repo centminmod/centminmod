4b8489c52377ccbec99506608922e49b8f72bac1
George Liu <eva2000@centminmod.com>
2016-10-28 07:58:06 +1000

    add addons/rclone.sh rsync client for cloud storage in 123.09beta01
    details at https://community.centminmod.com/threads/addons-rclone-sh-client-for-syncing-to-remote-cloud-storage-providers.9299/

---
+++ addons/rclone.sh
@@ -0,0 +1,262 @@
+#!/bin/bash
+###########################################################
+# rlone client installer http://rclone.org
+# written by George Liu centminmod.com
+# http://rclone.org/install/
+# http://rclone.org/docs/
+###########################################################
+DT=$(date +"%d%m%y-%H%M%S")
+CENTMINLOGDIR='/root/centminlogs'
+DIR_TMP='/svr-setup'
+BASEURL='http://downloads.rclone.org'
+
+###########################################################
+CENTOSVER=$(awk '{ print $3 }' /etc/redhat-release)
+
+if [ -f "/etc/centminmod/custom_config.inc" ]; then
+  # default is at /etc/centminmod/custom_config.inc
+  . "/etc/centminmod/custom_config.inc"
+fi
+
+if [[ "$RCLONE_ENABLE" != [yY] ]]; then
+  echo
+  echo "RCLONE_ENABLE='y' not set"
+  echo "during beta testing of addons/rclone.sh you need to"
+  echo "manually enable addons/rclone.sh via persistent config"
+  echo "file at /etc/centminmod/custom_config.inc and add to it"
+  echo
+  echo "to edit persistent config, type command: customconfig"
+  echo
+  echo "add to it and hit CTRL+X to save file"
+  echo
+  echo "RCLONE_ENABLE='y'"
+  echo
+  exit
+fi
+
+if [ ! -d "$CENTMINLOGDIR" ]; then
+  mkdir -p "$CENTMINLOGDIR"
+fi
+
+if [ "$CENTOSVER" == 'release' ]; then
+    CENTOSVER=$(awk '{ print $4 }' /etc/redhat-release | cut -d . -f1,2)
+    if [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '7' ]]; then
+        CENTOS_SEVEN='7'
+    fi
+fi
+
+if [[ "$(cat /etc/redhat-release | awk '{ print $3 }' | cut -d . -f1)" = '6' ]]; then
+    CENTOS_SIX='6'
+fi
+
+if [ "$CENTOSVER" == 'Enterprise' ]; then
+    CENTOSVER=$(cat /etc/redhat-release | awk '{ print $7 }')
+    OLS='y'
+fi
+
+if [ -f /proc/user_beancounters ]; then
+    # CPUS='1'
+    # MAKETHREADS=" -j$CPUS"
+    # speed up make
+    CPUS=$(grep -c "processor" /proc/cpuinfo)
+    if [[ "$CPUS" -gt '8' ]]; then
+        CPUS=$(echo "$CPUS+2" | bc)
+    else
+        CPUS=$(echo "$CPUS+1" | bc)
+    fi
+    MAKETHREADS=" -j$CPUS"
+else
+    # speed up make
+    CPUS=$(grep -c "processor" /proc/cpuinfo)
+    if [[ "$CPUS" -gt '8' ]]; then
+        CPUS=$(echo "$CPUS+2" | bc)
+    else
+        CPUS=$(echo "$CPUS+1" | bc)
+    fi
+    MAKETHREADS=" -j$CPUS"
+fi
+
+###########################################################
+# Setup Colours
+black='\E[30;40m'
+red='\E[31;40m'
+green='\E[32;40m'
+yellow='\E[33;40m'
+blue='\E[34;40m'
+magenta='\E[35;40m'
+cyan='\E[36;40m'
+white='\E[37;40m'
+
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+boldmagenta='\E[1;35;40m'
+boldcyan='\E[1;36;40m'
+boldwhite='\E[1;37;40m'
+
+Reset="tput sgr0"      #  Reset text attributes to normal
+                       #+ without clearing screen.
+
+cecho ()                     # Coloured-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+message=$1
+color=$2
+echo -e "$color$message" ; $Reset
+return
+}
+
+###########################################################
+rclone_config() {
+  echo
+  echo "------------------------------------------------"
+  echo "You need to now manually configure your storage provider"
+  echo "from instructions at http://rclone.org/docs/"
+  echo "------------------------------------------------"
+  echo
+  sleep 3
+  if [ -f /usr/sbin/rclone ]; then
+    rclone config
+  fi
+}
+
+rclone_install() {
+  if [[ "$(uname -m)" = 'x86_64' ]]; then
+      echo "------------------------------------------------"
+      echo "Install Rclone 64bit"
+      echo "------------------------------------------------"
+      cd "$DIR_TMP"
+      rm -rf rclone-*
+      wget -cnv -O rclone-current-linux-amd64.zip "${BASEURL}/rclone-current-linux-amd64.zip"
+      unzip rclone-current-linux-amd64.zip
+      cd rclone-*-linux-amd64
+      \cp -f rclone /usr/sbin/
+      if [ -f /usr/sbin/rclone ]; then
+        chown root:root /usr/sbin/rclone
+        chmod 755 /usr/sbin/rclone
+        mkdir -p /usr/local/share/man/man1
+        \cp -f rclone.1 /usr/local/share/man/man1/
+        mandb >/dev/null 2>&1
+        ls -lah /usr/sbin/rclone
+        echo
+        echo "------------------------------------------------"
+        echo "rclone binary at /usr/sbin/rclone"
+        echo "------------------------------------------------"
+      fi
+  else
+      echo "------------------------------------------------"
+      echo "Install Rclone 32bit"
+      echo "------------------------------------------------"
+      cd "$DIR_TMP"
+      rm -rf rclone-*
+      wget -cnv -O rclone-current-linux-386.zip "${BASEURL}/rclone-current-linux-386.zip"
+      unzip rclone-current-linux-386.zip
+      cd rclone-*-linux-386
+      \cp -f rclone /usr/sbin/
+      if [ -f /usr/sbin/rclone ]; then
+        chown root:root /usr/sbin/rclone
+        chmod 755 /usr/sbin/rclone
+        mkdir -p /usr/local/share/man/man1
+        \cp -f rclone.1 /usr/local/share/man/man1/
+        mandb >/dev/null 2>&1
+        ls -lah /usr/sbin/rclone
+        echo
+        echo "------------------------------------------------"
+        echo "rclone binary at /usr/sbin/rclone"
+        echo "------------------------------------------------"
+        # echo
+      fi
+  fi # centos 6 only needed
+}
+
+rclone_copy() {
+  echo "not yet implemented..."
+  echo "https://community.centminmod.com/posts/39071/"
+}
+
+rclone_sync() {
+  echo "not yet implemented..."
+  echo "https://community.centminmod.com/posts/39071/"
+}
+
+###########################################################################
+case $1 in
+  config)
+starttime=$(date +%s.%N)
+{
+  rclone_config
+} 2>&1 | tee "${CENTMINLOGDIR}/rclone_config_${DT}.log"
+
+endtime=$(date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> "${CENTMINLOGDIR}/rclone_config_${DT}.log"
+echo "Total Rclone Config Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/rclone_config_${DT}.log"
+echo
+tail -1 "${CENTMINLOGDIR}/rclone_config_${DT}.log"
+  ;;
+  install)
+starttime=$(date +%s.%N)
+{
+  rclone_install
+  rclone_config
+} 2>&1 | tee "${CENTMINLOGDIR}/rclone_installer_${DT}.log"
+
+endtime=$(date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> "${CENTMINLOGDIR}/rclone_installer_${DT}.log"
+echo "Total Rclone Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/rclone_installer_${DT}.log"
+echo
+tail -1 "${CENTMINLOGDIR}/rclone_installer_${DT}.log"
+  ;;
+  update)
+starttime=$(date +%s.%N)
+{
+  rclone_install
+} 2>&1 | tee "${CENTMINLOGDIR}/rclone_update_${DT}.log"
+
+endtime=$(date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> "${CENTMINLOGDIR}/rclone_update_${DT}.log"
+echo "Total Rclone Update Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/rclone_update_${DT}.log"
+echo
+tail -1 "${CENTMINLOGDIR}/rclone_update_${DT}.log"
+  ;;
+  copy)
+starttime=$(date +%s.%N)
+{
+  rclone_copy
+} 2>&1 | tee "${CENTMINLOGDIR}/rclone_copy_${DT}.log"
+
+endtime=$(date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> "${CENTMINLOGDIR}/rclone_copy_${DT}.log"
+echo "Total Rclone Copy Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/rclone_copy_${DT}.log"
+echo
+tail -1 "${CENTMINLOGDIR}/rclone_copy_${DT}.log"
+  ;;
+  sync)
+starttime=$(date +%s.%N)
+{
+  rclone_sync
+} 2>&1 | tee "${CENTMINLOGDIR}/rclone_sync_${DT}.log"
+
+endtime=$(date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> "${CENTMINLOGDIR}/rclone_sync_${DT}.log"
+echo "Total Rclone Sync Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/rclone_sync_${DT}.log"
+echo
+tail -1 "${CENTMINLOGDIR}/rclone_sync_${DT}.log"
+  ;;
+  *)
+    echo "$0 {install|update|copy|sync}"
+  ;;
+esac
+exit
