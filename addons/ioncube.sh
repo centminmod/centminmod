068676a00da09556e3cd90de165e81fdcd31d283
George Liu <eva2000@centminmod.com>
2016-10-29 05:01:51 +1000

    backport addons/ioncube.sh from 123.09beta01
    backport addons/ioncube.sh version from 123.09beta01 to 123.08stable for php 7 support

--- addons/ioncube.sh
+++ addons/ioncube.sh
@@ -9,15 +9,16 @@ PHPCURRENTVER=$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1,2)
 
 if [[ "$(expr $PHPCURRENTVER \= 5.7)" = 1 || "$(expr $PHPCURRENTVER \< 5.3)" = 1 ]]; then
   echo "Your current PHP version $PHPCURRENTVER is incompatible with ioncube loader"
-  echo "ioncube loader only supports PHP versions 5.3-5.6 currently"
+  echo "ioncube loader only supports PHP versions 5.3-5.6 & 7 currently"
   echo "aborting installation"
   exit
 fi
 
 echo
 echo "ioncube loader installation started"
-echo "ioncube loader only supports PHP 5.3, 5.4, 5.5 and 5.6"
-echo "ioncube loader does not support PHP 7.0 currently"
+echo "ioncube loader only supports PHP 5.3, 5.4, 5.5, 5.6 & 7.0"
+# echo "ioncube loader PHP 7 currently beta supported"
+echo "http://blog.ioncube.com/2016/09/15/php-7-ioncube-loaders/"
 echo
 
 cd /svr-setup
@@ -25,11 +26,31 @@ mkdir -p ioncube
 cd ioncube
 
 if [[ "$(uname -m)" = 'x86_64' ]]; then
-  wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
-  tar xvzf ioncube_loaders_lin_x86-64.tar.gz
+  if [[ "$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1)" != '7' ]]; then
+    if [[ "$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1,2)" != '5.6' ]]; then
+      wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64_5.1.2.tar.gz
+      tar xvzf ioncube_loaders_lin_x86-64_5.1.2.tar.gz
+    elif [[ "$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1,2)" = '5.6' ]]; then
+      wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+      tar xvzf ioncube_loaders_lin_x86-64.tar.gz
+    fi
+  else
+    wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+    tar xvzf ioncube_loaders_lin_x86-64.tar.gz
+  fi
 else
-  wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz
-  tar xvzf ioncube_loaders_lin_x86.tar.gz
+  if [[ "$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1)" != '7' ]]; then
+    if [[ "$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1,2)" != '5.6' ]]; then
+      wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86_5.1.2.tar.gz
+      tar xvzf ioncube_loaders_lin_x86_5.1.2.tar.gz
+    elif [[ "$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1,2)" = '5.6' ]]; then
+      wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz
+      tar xvzf ioncube_loaders_lin_x86.tar.gz
+    fi
+  else
+    wget -cnv http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz
+    tar xvzf ioncube_loaders_lin_x86.tar.gz
+  fi
 fi
 
 # check current PHP version
@@ -37,7 +58,17 @@ ICPHPVER=$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1,2)
 PHPEXTDIRD=`cat /usr/local/bin/php-config | awk '/^extension_dir/ {extdir=$1} END {gsub(/\047|extension_dir|=|)/,"",extdir); print extdir}'`
 
 # move current ioncube version to existing PHP extension directory
-\cp -f ioncube/ioncube_loader_lin_${ICPHPVER}.so ${PHPEXTDIRD}/ioncube.so
+if [[ "$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1)" != '7' ]]; then
+  \cp -f ioncube/ioncube_loader_lin_${ICPHPVER}.so "${PHPEXTDIRD}/ioncube.so"
+else
+  # for php 7 ioncube beta8
+  ICPHPVER=$(php -v | awk -F " " '{print $2}' | head -n1 | cut -d . -f1)
+  if [[ "$(uname -m)" = 'x86_64' ]]; then
+    \cp -f ioncube/ioncube_loader_lin_${ICPHPVER}.so "${PHPEXTDIRD}/ioncube.so"
+  else
+    \cp -f ioncube/ioncube_loader_lin_${ICPHPVER}.so "${PHPEXTDIRD}/ioncube.so"
+  fi
+fi
 
 CONFIGSCANDIR='/etc/centminmod/php.d'
 
@@ -55,14 +86,19 @@ ls -lah ${CONFIGSCANDIR}
 echo ""
 ls -lah ${PHPEXTDIRD}
 
-fpmrestart
+service php-fpm restart >/dev/null 2>&1
 
-echo ""
-echo "Check if PHP module: ionCube Loader loaded"
-php --ri 'ionCube Loader'
-
-echo
-echo "ioncube loader installation completed"
-echo "you'll need to rerun ioncube.sh after each major PHP version upgrades"
-echo "PHP 5.3 to 5.4 or PHP 5.4 to PHP 5.5 to PHP 5.6"
-echo
+if [ -f "${PHPEXTDIRD}/ioncube.so" ]; then
+  echo ""
+  echo "Check if PHP module: ionCube Loader loaded"
+  php --ri 'ionCube Loader'
+  
+  echo
+  echo "ioncube loader installation completed"
+  echo "you'll need to rerun ioncube.sh after each major PHP version upgrades"
+  echo "PHP 5.3 to 5.4 or PHP 5.4 to PHP 5.5 to PHP 5.6 to PHP 7.0 etc"
+  echo
+else
+  echo ""
+  echo "ionCube Loader failed to install properly"
+fi
