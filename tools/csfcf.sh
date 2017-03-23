13087af685ab7b346a3824c2f7c7fd54cf1740fa
George Liu <eva2000@centminmod.com>
2017-03-24 04:04:58 +1000

    another tools/csfcf.sh fix

--- tools/csfcf.sh
+++ tools/csfcf.sh
@@ -155,14 +155,14 @@ csfadd() {
 nginxsetup() {
 	echo
 	# echo "create $CFINCLUDEFILE include file"
+	if [ -f "$CFINCLUDEFILE" ]; then
+		\cp -af "$CFINCLUDEFILE" "${CFINCLUDEFILE}.bak"
+	fi
 	echo > $CFINCLUDEFILE
 	cflista=$(/usr/bin/curl -s ${CURL_TIMEOUTS} https://www.cloudflare.com/ips-v4/)
 	cflistb=$(/usr/bin/curl -s ${CURL_TIMEOUTS} https://www.cloudflare.com/ips-v6/)
 	if [ ! -f /usr/local/nginx/conf/cloudflare_customips.conf ]; then
 		touch /usr/local/nginx/conf/cloudflare_customips.conf
-	fi
-	if [ -f "$CFINCLUDEFILE" ]; then
-		\cp -af "$CFINCLUDEFILE" "${CFINCLUDEFILE}.bak"
 	fi
 	echo "include /usr/local/nginx/conf/cloudflare_customips.conf;" >> $CFINCLUDEFILE
 	for i in $cflista; do
