0304715c03f3ad0618a5d8c36b934151ecdf3e06
George Liu <eva2000@centminmod.com>
2017-05-15 14:45:50 +1000

    update addons/wp-cli.sh add wp-cli packages
    - add wp-cli packages wp-checksum https://github.com/eriktorsner/wp-checksum and wp-sec https://github.com/markri/wp-sec discussed at https://community.centminmod.com/threads/wordpress-checksum-verification.11692/

--- addons/wpcli.sh
+++ addons/wpcli.sh
@@ -84,7 +84,9 @@ else
 	fi
 fi
 
-chmod 0700 /usr/bin/wp
+if [ -f /usr/bin/wp ]; then
+  chmod 0700 /usr/bin/wp
+fi
 
 echo ""
 if [ -s "${WPCLIDIR}/wp-completion.bash" ]; then
@@ -115,7 +117,27 @@ if [[ -z "$WPCLICHECK" ]]; then
 	echo "source ${WPCLIDIR}/wp-completion.bash" >> /root/.bash_profile
 fi
 
+WPALIASCHECK=$(grep 'allow-root' /root/.bashrc)
+
+if [[ -z "$WPALIASCHECK" ]]; then
+  echo "alias wp='wp --allow-root'" >> /root/.bashrc
+fi
+
+if [[ "$(wp package list | grep -q 'eriktorsner\/wp-checksum'; echo $?)" -ne '0' ]]; then
+  echo "-------------------------------------------------------------"
+  echo "install wp-cli https://github.com/eriktorsner/wp-checksum"
+  /usr/bin/wp package install eriktorsner/wp-checksum --allow-root
+fi
+if [[ "$(wp package list | grep -q 'markri\/wp-sec'; echo $?)" -ne '0' ]]; then
+  echo "-------------------------------------------------------------"
+  echo "install wp-cli https://github.com/markri/wp-sec"
+  /usr/bin/wp package install markri/wp-sec --allow-root
+fi
 echo "-------------------------------------------------------------"
+echo "update wp-cli packages"
+/usr/bin/wp package update --allow-root
+echo "-------------------------------------------------------------"
+echo "wp-cli info"
 /usr/bin/wp --info --allow-root
 echo "-------------------------------------------------------------"
 
