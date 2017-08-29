8228704948c506ce490ba418508029169d922815
George Liu <eva2000@centminmod.com>
2017-08-30 08:44:48 +1000

    add updatecm.sh for auto updating centmin mod code
    - for 123.09beta01 and higher https://community.centminmod.com/threads/cronjob-auto-updating-centmin-mod-code-guide.3398/

---
+++ updatecm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#######################################################
+# centminmod.com updater
+# https://community.centminmod.com/threads/3398/
+# 
+# setup cron job i.e. every 6 hrs
+# 0 */6 * * * /usr/local/src/centminmod/updatecm.sh
+#######################################################
+export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
+DT=$(date +"%d%m%y-%H%M%S")
+
+update() {
+  if [ -d /usr/local/src/centminmod/.git ]; then
+  echo
+  echo "-------------------------------------"
+  echo "Updating Centmin Mod code"
+  echo "-------------------------------------"
+  echo
+    cd /usr/local/src/centminmod
+    git branch
+    git stash
+    git pull
+    git log -1 | sed -e 's|Author: George Liu <.*>|Author: George Liu <snipped>|g'
+  fi
+  echo
+  echo "-------------------------------------"
+  echo "Updated Centmin Mod code"
+  echo "-------------------------------------"
+  echo
+}
+
+starttime=$(TZ=UTC date +%s.%N)
+{
+update
+} 2>&1 | tee "${CENTMINLOGDIR}/updatecm_${DT}.log"
+
+endtime=$(TZ=UTC date +%s.%N)
+
+INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+echo "" >> "${CENTMINLOGDIR}/updatecm_${DT}.log"
+echo "Total updatecm.sh Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/updatecm_${DT}.log"
