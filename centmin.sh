dcdb9ff5343ae985afbc981a3769fa4233777eed
George Liu <eva2000@centminmod.com>
2017-11-11 16:29:46 +1000

    update mongodb php driver to 1.3.2
    https://pecl.php.net/package/mongodb

--- centmin.sh
+++ centmin.sh
@@ -588,7 +588,7 @@ REDISPHP_VER='3.1.4'        # redis PHP version for PHP <7.x
 REDISPHPSEVEN_VER='3.1.4'   # redis PHP version for PHP =>7.x
 REDISPHP_GIT='n'            # pull php 7 redis extension from git or pecl downloads
 PHPMONGODB='n'              # MongoDB PHP extension install
-MONGODBPHP_VER='1.1.8'      # MongoDB PHP version
+MONGODBPHP_VER='1.3.2'      # MongoDB PHP version
 MONGODB_SASL='n'            # SASL not working yet leave = n
 PDOPGSQL_PHPVER='9.6'       # pdo-pgsql PHP extension version for postgresql
 
