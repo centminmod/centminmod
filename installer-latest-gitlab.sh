b34b9fb0731b8b52286cc30b4f7b8030ef1d7b9b
George Liu <eva2000@centminmod.com>
2018-02-03 16:40:09 +1000

    update installers PHP 7.1.14 & 7.2.2 defaults

--- installer-latest-gitlab.sh
+++ installer-latest-gitlab.sh
@@ -1104,7 +1104,7 @@ cd $INSTALLDIR
 #sed -i "s|PHPREDIS='y'|PHPREDIS='n'|" centmin.sh
 
 # switch from PHP 5.4.41 to 5.6.9 default with Zend Opcache
-sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='7.1.13'|" centmin.sh
+sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='7.1.14'|" centmin.sh
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
 
 # disable axivo yum repo
