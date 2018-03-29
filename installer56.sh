e3dd038b5cb3a2a8b6d65816d72f15e4b8eb77b0
George Liu <eva2000@centminmod.com>
2018-03-30 01:06:58 +1000

    update PHP 5.6.35 for 123.09beta01 installers

--- installer56.sh
+++ installer56.sh
@@ -1132,7 +1132,7 @@ cd $INSTALLDIR
 #sed -i "s|PHPREDIS='y'|PHPREDIS='n'|" centmin.sh
 
 # switch from PHP 5.4.41 to 5.6.9 default with Zend Opcache
-sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='5.6.34'|" centmin.sh
+sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='5.6.35'|" centmin.sh
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
 
 # disable axivo yum repo
