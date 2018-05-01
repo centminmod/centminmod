e5359afe21cdb44ff04440cd2526ca4a79d00601
George Liu <eva2000@centminmod.com>
2018-05-01 17:05:10 +1000

    update 123.09beta01 git update routines to check gitclean.txt
    - Update both centmin mod cmupdate and centmin.sh menu option 23 submenu option 2 to check whether or not Github repository has been cleaned and history rewritten due to maintenance tasks needed in the future. For example, reducing size of Github repo will involve rewriting the git repositories commit history.
    - Normal git pull updates won't work after commit history is rewritten so these updated changes will allow cmupdate.sh and centmin.sh menu option 23 submenu option 2 to check the gitclean.txt control file hosted on github.com repo to determine if a git pull is adequate or if a fresh git clone is required.

--- tools/cmupdate.sh
+++ tools/cmupdate.sh
@@ -14,8 +14,7 @@ fi
 
 # variables
 #############
-
-
+branchname=123.09beta01
 DT=$(date +"%d%m%y-%H%M%S")
 ######################################################
 # functions
@@ -29,9 +28,24 @@ export LC_CTYPE=en_US.UTF-8
 
 fupdate() {
   if [[ -d "${CM_INSTALLDIR}/.git" ]]; then
-    cd "${CM_INSTALLDIR}"
-    git stash
-    git pull
+    if [[ "$(curl -sL https://github.com/centminmod/centminmod/raw/${branchname}/gitclean.txt)" = 'no' ]]; then
+      cd "${CM_INSTALLDIR}"
+      git stash
+      git pull
+    else
+      echo
+      echo "Detected Centmin Mod Github Remote Repo Changes"
+      echo "setting up fresh /usr/local/src/centminmod code base to match"
+      echo
+      rm -rf "${CM_INSTALLDIR}"
+      cd /usr/local/src
+      git clone -b ${branchname} --depth=1 https://github.com/centminmod/centminmod.git centminmod
+      echo
+      echo "Completed. Fresh /usr/local/src/centminmod code base in place"
+      echo "To run centmin.sh again, you need to change into directory: ${CM_INSTALLDIR}"
+      echo "cd ${CM_INSTALLDIR}"
+      echo
+    fi
   fi
 }
 
