06929f57b8a7f8b7ee3f08eef474fe7666a8de8f
George Liu <eva2000@centminmod.com>
2018-05-26 00:14:59 +1000

    update alternate beta installers default PHP versions
    PHP 7.2.6 http://au2.php.net/ChangeLog-7.php#7.2.6
    PHP 7.1.18 http://au2.php.net/ChangeLog-7.php#7.1.18
    PHP 7.0.30 http://au2.php.net/ChangeLog-7.php#7.0.30

--- installer72.sh
+++ installer72.sh
@@ -1137,7 +1137,8 @@ cd $INSTALLDIR
 #sed -i "s|PHPREDIS='y'|PHPREDIS='n'|" centmin.sh
 
 # switch from PHP 5.4.41 to 5.6.9 default with Zend Opcache
-sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='7.2.5'|" centmin.sh
+PHPVERLATEST=$(curl -s http://php.net/downloads.php | egrep -o "php\-[0-9.]+\.tar[.a-z]*" | grep -v '.asc' | awk -F "php-" '/.tar.gz$/ {print $2}' | sed -e 's|.tar.gz||g' | uniq | grep '7.2' | head -n1)
+sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='$PHPVERLATEST'|" centmin.sh
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
 
 # disable axivo yum repo
