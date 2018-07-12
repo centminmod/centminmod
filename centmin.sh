f7c9af50d7ed9f7839d3ce77b1ad2c68bdf455b7
George Liu <eva2000@centminmod.com>
2018-07-12 22:00:41 +1000

    123.09beta01 update php 7 redis 4.1.0 & mongodb 1.5.1 extensions

--- centmin.sh
+++ centmin.sh
@@ -676,10 +676,10 @@ LIBMEMCACHED_VER='1.0.18'   # libmemcached version for source compile
 TWEMPERF_VER='0.1.1'
 PHPREDIS='y'                # redis PHP extension install
 REDISPHP_VER='3.1.6'        # redis PHP version for PHP <7.x
-REDISPHPSEVEN_VER='3.1.6'   # redis PHP version for PHP =>7.x
+REDISPHPSEVEN_VER='4.1.0'   # redis PHP version for PHP =>7.x
 REDISPHP_GIT='n'            # pull php 7 redis extension from git or pecl downloads
 PHPMONGODB='n'              # MongoDB PHP extension install
-MONGODBPHP_VER='1.4.3'      # MongoDB PHP version
+MONGODBPHP_VER='1.5.1'      # MongoDB PHP version
 MONGODB_SASL='n'            # SASL not working yet leave = n
 PDOPGSQL_PHPVER='9.6'       # pdo-pgsql PHP extension version for postgresql
 PHP_LIBZIP='n'              # use newer libzip instead of PHP embedded zip
