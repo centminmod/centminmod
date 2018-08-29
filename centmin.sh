c740ca2ea3708dbe1fcee2125d0d9f564ec66897
George Liu <eva2000@centminmod.com>
2018-08-29 13:46:43 +1000

    nginx 1.15.3 & nginx openresty modules no longer optional

--- centmin.sh
+++ centmin.sh
@@ -21,7 +21,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='123.09beta01'
 SCRIPT_MAJORVER='1.2.3'
 SCRIPT_MINORVER='09'
-SCRIPT_INCREMENTVER='052'
+SCRIPT_INCREMENTVER='053'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='31/09/2018'
@@ -644,7 +644,7 @@ MYSQL_INSTALL='n'            # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL='n'         # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.15.2'       # Use this version of Nginx
+NGINX_VERSION='1.15.3'       # Use this version of Nginx
 NGINX_VHOSTSSL='y'           # enable centmin.sh menu 2 prompt to create self signed SSL vhost 2nd vhost conf
 NGINXBACKUP='y'
 VHOST_PRESTATICINC='y'       # add pre-staticfiles-local.conf & pre-staticfiles-global.conf include files
@@ -1080,6 +1080,13 @@ fi
 if [[ "$ORESTY_LUANGINX" = [yY] ]]; then
     NGINX_OPENRESTY='y'
     LIBRESSL_SWITCH='n'
+fi
+
+if [[ "$NGINX_OPENRESTY" = [nN] ]]; then
+  # ensure openresty modules are installed as some are 
+  # no longer optional and required for Centmin Mod 
+  # nginx functionality i.e. wordpress caching configurations
+  NGINX_OPENRESTY='y'
 fi
 
 if [[ "$(uname -m)" = 'x86_64' ]]; then
