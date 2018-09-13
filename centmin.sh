9e1881c1e35949bc33802e2f9933a8835926f82a
George Liu <eva2000@centminmod.com>
2018-09-14 04:41:26 +1000

    php 5.6.38 default for 123.09beta01
    https://community.centminmod.com/threads/php-5-6-38-7-0-32-7-1-22-7-2-10-security-bug-fix-updates.15620/

--- centmin.sh
+++ centmin.sh
@@ -720,7 +720,7 @@ LIBMEMCACHED_VER='1.0.18'   # libmemcached version for source compile
 TWEMPERF_VER='0.1.1'
 
 PHP_OVERWRITECONF='y'       # whether to show the php upgrade prompt to overwrite php-fpm.conf
-PHP_VERSION='5.6.37'        # Use this version of PHP
+PHP_VERSION='5.6.38'        # Use this version of PHP
 PHP_MIRRORURL='http://php.net'
 PHPUPGRADE_MIRRORURL="$PHP_MIRRORURL"
 XCACHE_VERSION='3.2.0'      # Use this version of Xcache
