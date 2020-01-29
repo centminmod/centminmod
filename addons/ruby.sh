322bcb83b114faa8b1d09d9cc7bdb47f7459c8a3
George Liu <eva2000@centminmod.com>
2020-01-30 07:05:28 +1000

    update addons/ruby.sh to 2.7.0 in 123.09beta01

--- addons/ruby.sh
+++ addons/ruby.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VER='0.0.2'
+VER='0.0.3'
 #####################################################
 # set locale temporarily to english
 # due to some non-english locale issues
@@ -13,7 +13,7 @@ export LC_CTYPE=en_US.UTF-8
 # written by George Liu (eva2000) centminmod.com
 # https://rvm.io/
 ######################################################
-RUBYVER='2.6.0'
+RUBYVER='2.7.0'
 RUBYBUILD=''
 
 DT=$(date +"%d%m%y-%H%M%S")
