3cb25c351a4ad016bb1fcb9ac10c81c3f124c930
George Liu <eva2000@centminmod.com>
2020-04-10 14:23:43 +1000

    Update REMI YUM ImageMagick6 package name obsoleting in 123.09beta01
    - For CentOS 7, ImageMagick6 package names obsoleted and renamed to/by ImageMagick package name in REMI yum repo so need to update all detection routines which differentiate and install REMI yum repo's ImageMagick version versus native CentOS imageMagick name
    - While CentOS 6, REMI YUM repo still uses ImageMagick6 package names for now

--- centmin.sh
+++ centmin.sh
@@ -27,7 +27,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='123.09beta01'
 SCRIPT_MAJORVER='1.2.3'
 SCRIPT_MINORVER='09'
-SCRIPT_INCREMENTVER='486'
+SCRIPT_INCREMENTVER='487'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='31/03/2020'
@@ -697,7 +697,7 @@ SHORTCUTS='y'                # shortcuts
 POSTGRESQL='n'               # set to =y to install PostgreSQL 9.6 server, devel packages and pdo-pgsql PHP extension
 POSTGRESQL_BRANCHVER='11'   # PostgresSQL branch version https://www.postgresql.org/ i.e. 9.6, 10 or 11
 
-IMAGEMAGICK_HEIF='n'         # experimental ImageMagick6 HEIF image format support
+IMAGEMAGICK_HEIF='n'         # experimental ImageMagick HEIF image format support
 ########################################################
 # Choice of installing MariaDB 5.2 via RPM or via MariaDB 5.2 CentOS YUM Repo
 # If MDB_YUMREPOINSTALL=y and MDB_INSTALL=n then MDB_VERONLY version 
