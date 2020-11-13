bfe372edb14fc409eeae19ea5a945f6f80541855
George Liu <eva2000@centminmod.com>
2020-11-14 04:01:09 +1000

    Update PHP 7+ redis PHP extension to 5.3.2 in 123.09beta01
    - Update redis PHP extension to 5.3.2 https://pecl.php.net/redis
    - Existing users can run cmupdate before centmin.sh menu option 13 submenu option 2 update routine for redis

--- centmin.sh
+++ centmin.sh
@@ -27,7 +27,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='123.09beta01'
 SCRIPT_MAJORVER='1.2.3'
 SCRIPT_MINORVER='09'
-SCRIPT_INCREMENTVER='614'
+SCRIPT_INCREMENTVER='615'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='31/12/2020'
@@ -685,7 +685,7 @@ SUHOSINVER='0.9.38'
 
 PHPREDIS='y'                # redis PHP extension install
 REDISPHP_VER='4.3.0'        # redis PHP version for PHP <7.x
-REDISPHPSEVEN_VER='5.3.1'   # redis PHP version for PHP =>7.x
+REDISPHPSEVEN_VER='5.3.2'   # redis PHP version for PHP =>7.x
 REDISPHP_GIT='n'            # pull php 7 redis extension from git or pecl downloads
 PHPMONGODB='n'              # MongoDB PHP extension install
 MONGODBPHP_VER='1.7.4'      # MongoDB PHP version
