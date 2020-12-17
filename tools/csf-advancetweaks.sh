8eb9c0f8be2e65964d713eb34e02510849d2da60
George Liu <eva2000@centminmod.com>
2020-12-17 20:12:27 +1000

    part 4 improve LXD container 4.9 compatibility in 123.09beta01

--- tools/csf-advancetweaks.sh
+++ tools/csf-advancetweaks.sh
@@ -8,7 +8,7 @@
 DT=$(date +"%d%m%y-%H%M%S")
 
 denyiplimits() {
-if [[ ! -f /proc/user_beancounters && -f /usr/sbin/ipset ]] && [[ "$(uname -r | grep linode)" || "$(find /lib/modules/`uname -r` -name 'ipset')" ]]; then
+if [[ ! -f /proc/user_beancounters && -f /usr/sbin/ipset ]] && [[ "$(uname -r | grep linode)" || "$(find /lib/modules/`uname -r` -name 'ipset')" ]] && [[ "$(virt-what | grep -o lxc)" != 'lxc' ]]; then
   echo
   echo "CSF Firewall dynamically optimise DENY_IP_LIMIT"
   echo "and DENY_TEMP_IP_LIMIT based on system resources"
