425d601f3cfc02e73743ea72a0e78cd68d401e37
George Liu <eva2000@centminmod.com>
2021-05-07 14:49:25 +1000

    reverse ImageMagick6 obsoleting change in 123.09beta01
    - reverse the change for CentOS 7 systems for REMI YUM repo ImageMagick6 to ImageMagick package name change https://community.centminmod.com/threads/update-remi-yum-imagemagick6-package-name-obsoleting-in-123-09beta01.19485/ back to using ImageMagick6 package naming scheme to be able to access ImageMagick 6.9.12.* versions. Seems REMI switched back to ImageMagick6 package names to be able to move and upgrade beyond CentOS 7.8+ system ImageMagick 6.9.11.x versions to newer ImageMacgik 6.9.12.x versions again.
    - for existing users, run cmupdate command and then run centmin.sh menu option 15 to do the update. To verify the updated ImageMagick6 6.9.12.x packages are installed run command below
    
    yum -q list installed ImageMagick6*
    
    You should see the following packages installed
    
    ImageMagick6.x86_64            6.9.12.11-1.el7.remi  @remi
    ImageMagick6-c++.x86_64        6.9.12.11-1.el7.remi  @remi
    ImageMagick6-c++-devel.x86_64  6.9.12.11-1.el7.remi  @remi
    ImageMagick6-devel.x86_64      6.9.12.11-1.el7.remi  @remi
    ImageMagick6-libs.x86_64       6.9.12.11-1.el7.remi  @remi

--- tools/imagemagick-update.sh
+++ tools/imagemagick-update.sh
@@ -268,7 +268,7 @@ checkimagicksys() {
       if [[ "$CENTOS_SIX" = '6' ]]; then
         REMI_IMAGEMAGICK_PKGNAME='6'
       else
-        REMI_IMAGEMAGICK_PKGNAME=""
+        REMI_IMAGEMAGICK_PKGNAME="6"
       fi
 
     cecho "Check for ImageMagicK System Updates (YUM)" $boldyellow
@@ -278,7 +278,7 @@ checkimagicksys() {
                 # skip for initial installs to speed up install
                 if [[ "$INITIALINSTALL" != [yY] ]]; then
                     yum clean all >/dev/null 2>&1
-                    yum -y update ImageMagick6 ImageMagick6-devel ImageMagick6-c++ ImageMagick6-c++-devel --enablerepo=remi --disableplugin=priorities
+                    yum -y update ImageMagick${REMI_IMAGEMAGICK_PKGNAME} ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-devel ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++ ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++-devel --enablerepo=remi --disableplugin=priorities
                 fi
             else
                 if [[ "$CENTOS_SIX" = '6' ]]; then
@@ -287,7 +287,7 @@ checkimagicksys() {
                     yum -y install ImageMagick${REMI_IMAGEMAGICK_PKGNAME} ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-devel ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++ ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++-devel --enablerepo=remi --disableplugin=priorities --skip-broken
                 else
                     yum clean all >/dev/null 2>&1
-                    yum -y install ImageMagick ImageMagick-devel ImageMagick-c++ ImageMagick-c++-devel --enablerepo=remi --disableplugin=priorities
+                    yum -y install ImageMagick${REMI_IMAGEMAGICK_PKGNAME} ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-devel ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++ ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++-devel --enablerepo=remi --disableplugin=priorities
                 fi
             fi
         elif [ ! -f /etc/yum.repos.d/remi.repo ]; then
@@ -297,7 +297,7 @@ checkimagicksys() {
             elif [[ "$CENTOS_SEVEN" = '7' ]]; then
                 remisevenyum
             fi
-            if [[ $(rpm -q ImageMagick >/dev/null 2>&1; echo $?) = '0' ]]; then
+            if [[ $(rpm -q ImageMagick${REMI_IMAGEMAGICK_PKGNAME} >/dev/null 2>&1; echo $?) = '0' ]]; then
                 echo
                 echo "----------------------------------------------------------------------------------"
                 cecho "Using Remi YUM repo ImageMagicK version" $boldyellow
@@ -309,7 +309,7 @@ checkimagicksys() {
                     # yum -y install libwebp libwebp-devel --skip-broken
                     yum -y install ImageMagick${REMI_IMAGEMAGICK_PKGNAME} ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-devel ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++ ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++-devel --enablerepo=remi --disableplugin=priorities --skip-broken
                 else
-                    yum -y install ImageMagick ImageMagick-devel ImageMagick-c++ ImageMagick-c++-devel --enablerepo=remi --disableplugin=priorities
+                    yum -y install ImageMagick${REMI_IMAGEMAGICK_PKGNAME} ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-devel ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++ ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++-devel --enablerepo=remi --disableplugin=priorities
                 fi
                 echo
             else
@@ -327,7 +327,7 @@ checkimagicksys() {
                     # yum -y install libwebp libwebp-devel --skip-broken
                     yum -y install ImageMagick${REMI_IMAGEMAGICK_PKGNAME} ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-devel ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++ ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++-devel --enablerepo=remi --disableplugin=priorities --skip-broken
                 else
-                    yum -y install ImageMagick ImageMagick-devel ImageMagick-c++ ImageMagick-c++-devel --enablerepo=remi --disableplugin=priorities
+                    yum -y install ImageMagick${REMI_IMAGEMAGICK_PKGNAME} ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-devel ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++ ImageMagick${REMI_IMAGEMAGICK_PKGNAME}-c++-devel --enablerepo=remi --disableplugin=priorities
                 fi
             fi
         fi
