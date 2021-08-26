3ea6fb6e31d07e73f961c0fc2cf5bed5d5192f25
George Liu <eva2000@centminmod.com>
2021-08-27 05:04:37 +1000

    update default PHP versions in installers in 123.09beta01
    - update installers default PHP to 7.3.30, 7.4.23 or 8.0.10 respectively for betainstaller73.sh, betainstaller74.sh and betainstaller80.sh

--- installer80.sh
+++ installer80.sh
@@ -1379,7 +1379,7 @@ cd $INSTALLDIR
 
 # switch from PHP 5.4.41 to 5.6.9 default with Zend Opcache
 PHPVERLATEST=$(curl -${ipv_forceopt}sL https://www.php.net/downloads.php| egrep -o "php\-[0-9.]+\.tar[.a-z]*" | grep -v '.asc' | awk -F "php-" '/.tar.gz$/ {print $2}' | sed -e 's|.tar.gz||g' | uniq | grep '8.0' | head -n1)
-PHPVERLATEST=${PHPVERLATEST:-"8.0.8"}
+PHPVERLATEST=${PHPVERLATEST:-"8.0.10"}
 sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='$PHPVERLATEST'|" centmin.sh
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
 
