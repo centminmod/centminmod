72c724487fbbbc7ff4c8a4af533fb719cbb48938
George Liu (eva2000) <eva2000@centminmod.com>
2022-03-29 03:56:36 +1000

    update AMD EPYC Rome & Milan detection

--- installer80.sh
+++ installer80.sh
@@ -339,6 +339,93 @@ if [ -f /proc/user_beancounters ]; then
             # 7371 at 8 cpu cores has 3.8Ghz clock frequency https://en.wikichip.org/wiki/amd/epyc/7371
             # while greater than 8 cpu cores downclocks to 3.6Ghz
             CPUS=8
+        elif [[ "$(grep -o 'AMD EPYC 7272' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7272' ]]; then
+            # 7272 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7272
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7282' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7282' ]]; then
+            # 7282 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7282
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7302' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7302' ]]; then
+            # 7302 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7302
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7352' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7352' ]]; then
+            # 7352 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7352
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7402' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7402' ]]; then
+            # 7402 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7402
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7452' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7452' ]]; then
+            # 7452 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7452
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7502' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7502' ]]; then
+            # 7502 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7502
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7532' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7532' ]]; then
+            # 7532 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7532
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7542' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7542' ]]; then
+            # 7542 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7542
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7552' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7552' ]]; then
+            # 7552 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7552
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7642' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7642' ]]; then
+            # 7642 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7642
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7662' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7662' ]]; then
+            # 7662 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7662
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7702' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7702' ]]; then
+            # 7702 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7702
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7742' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7742' ]]; then
+            # 7742 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7742
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7H12' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7H12' ]]; then
+            # 7H12 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7H12
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7F52' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7F52' ]]; then
+            # 7F52 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7F52
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7F72' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7F72' ]]; then
+            # 7F72 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7F72
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7313' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7313' ]]; then
+            # 7313 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7313
+            CPUS=8
+        elif [[ "$(grep -o 'AMD EPYC 7413' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7413' ]]; then
+            # 7413 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7413
+            CPUS=12
+        elif [[ "$(grep -o 'AMD EPYC 7443' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7443' ]]; then
+            # 7443 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7443
+            CPUS=12
+        elif [[ "$(grep -o 'AMD EPYC 7453' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7453' ]]; then
+            # 7453 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7453
+            CPUS=14
+        elif [[ "$(grep -o 'AMD EPYC 7513' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7513' ]]; then
+            # 7513 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7513
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7543' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7543' ]]; then
+            # 7543 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7543
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7643' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7643' ]]; then
+            # 7643 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7643
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7663' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7663' ]]; then
+            # 7663 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7663
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7713' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7713' ]]; then
+            # 7713 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7713
+            CPUS=32
+        elif [[ "$(grep -o 'AMD EPYC 73F3' /proc/cpuinfo | sort -u)" = 'AMD EPYC 73F3' ]]; then
+            # 73F3 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/73F3
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 74F3' /proc/cpuinfo | sort -u)" = 'AMD EPYC 74F3' ]]; then
+            # 74F3 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/74F3
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 75F3' /proc/cpuinfo | sort -u)" = 'AMD EPYC 75F3' ]]; then
+            # 75F3 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/75F3
+            CPUS=32
         else
             CPUS=$(echo $(($CPUS+2)))
         fi
@@ -374,6 +461,93 @@ else
             # 7371 at 8 cpu cores has 3.8Ghz clock frequency https://en.wikichip.org/wiki/amd/epyc/7371
             # while greater than 8 cpu cores downclocks to 3.6Ghz
             CPUS=8
+        elif [[ "$(grep -o 'AMD EPYC 7272' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7272' ]]; then
+            # 7272 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7272
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7282' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7282' ]]; then
+            # 7282 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7282
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7302' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7302' ]]; then
+            # 7302 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7302
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7352' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7352' ]]; then
+            # 7352 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7352
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7402' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7402' ]]; then
+            # 7402 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7402
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7452' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7452' ]]; then
+            # 7452 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7452
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7502' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7502' ]]; then
+            # 7502 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7502
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7532' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7532' ]]; then
+            # 7532 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7532
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7542' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7542' ]]; then
+            # 7542 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7542
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7552' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7552' ]]; then
+            # 7552 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7552
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7642' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7642' ]]; then
+            # 7642 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7642
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7662' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7662' ]]; then
+            # 7662 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7662
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7702' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7702' ]]; then
+            # 7702 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7702
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7742' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7742' ]]; then
+            # 7742 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7742
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 7H12' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7H12' ]]; then
+            # 7H12 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7H12
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7F52' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7F52' ]]; then
+            # 7F52 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7F52
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7F72' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7F72' ]]; then
+            # 7F72 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7F72
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7313' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7313' ]]; then
+            # 7313 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7313
+            CPUS=8
+        elif [[ "$(grep -o 'AMD EPYC 7413' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7413' ]]; then
+            # 7413 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7413
+            CPUS=12
+        elif [[ "$(grep -o 'AMD EPYC 7443' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7443' ]]; then
+            # 7443 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7443
+            CPUS=12
+        elif [[ "$(grep -o 'AMD EPYC 7453' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7453' ]]; then
+            # 7453 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7453
+            CPUS=14
+        elif [[ "$(grep -o 'AMD EPYC 7513' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7513' ]]; then
+            # 7513 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7513
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7543' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7543' ]]; then
+            # 7543 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7543
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7643' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7643' ]]; then
+            # 7643 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7643
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7663' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7663' ]]; then
+            # 7663 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7663
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 7713' /proc/cpuinfo | sort -u)" = 'AMD EPYC 7713' ]]; then
+            # 7713 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/7713
+            CPUS=32
+        elif [[ "$(grep -o 'AMD EPYC 73F3' /proc/cpuinfo | sort -u)" = 'AMD EPYC 73F3' ]]; then
+            # 73F3 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/73F3
+            CPUS=16
+        elif [[ "$(grep -o 'AMD EPYC 74F3' /proc/cpuinfo | sort -u)" = 'AMD EPYC 74F3' ]]; then
+            # 74F3 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/74F3
+            CPUS=24
+        elif [[ "$(grep -o 'AMD EPYC 75F3' /proc/cpuinfo | sort -u)" = 'AMD EPYC 75F3' ]]; then
+            # 75F3 preferring higher clock frequency https://en.wikichip.org/wiki/amd/epyc/75F3
+            CPUS=32
         else
             CPUS=$(echo $(($CPUS+4)))
         fi
