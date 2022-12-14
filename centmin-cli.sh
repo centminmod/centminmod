a0e28ee7c195f8ac019c49bfb786a8cf3fcc09c3
George Liu (eva2000) <eva2000@centminmod.com>
2022-12-14 10:03:20 +1000

    update nginx 1.23.3 default in 130.00beta01

--- centmin-cli.sh
+++ centmin-cli.sh
@@ -29,7 +29,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='130.00beta01'
 SCRIPT_MAJORVER='130'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='257'
+SCRIPT_INCREMENTVER='258'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='01/12/22'
@@ -822,8 +822,8 @@ NGINX_ZERODT='n'             # nginx zero downtime reloading on nginx upgrades
 NGINX_ONETWOTHREE_COMPAT='y' # whether to allow nginx 1.23+ installs
 NGINX_MAXERRBYTELIMIT='2048' # modify NGX_MAX_ERROR_STR hardcoded 2048 limit by editing value i.e. http://openresty-reference.readthedocs.io/en/latest/Lua_Nginx_API/#print
 NGINX_INSTALL='y'            # Install Nginx (Webserver)
-NGINX_HPACK_ALLOWED_VER='1022001'      # Max allowed Nginx version for Nginx HTTP/2 HPACK full encoding patch support
-NGINX_DYNAMICTLS_ALLOWED_VER='1022001' # Max allowed Nginx version for Nginx Dynamic TLS patch support
+NGINX_HPACK_ALLOWED_VER='1022005'      # Max allowed Nginx version for Nginx HTTP/2 HPACK full encoding patch support
+NGINX_DYNAMICTLS_ALLOWED_VER='1022005' # Max allowed Nginx version for Nginx Dynamic TLS patch support
 NGINX_DEBUG='n'              # Enable & reinstall Nginx debug log nginx.org/en/docs/debugging_log.html & wiki.nginx.org/Debugging
 NGINX_HTTP2='y'              # Nginx http/2 patch https://community.centminmod.com/threads/4127/
 NGINX_KTLS='n'               # Enable Nginx kTLS - TLS in Kernel support if OpenSSL 3.0.x & 5.2+ Kernel detected
@@ -1064,7 +1064,7 @@ MYSQL_INSTALL='n'            # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL='n'         # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.23.2'       # Use this version of Nginx
+NGINX_VERSION='1.23.3'       # Use this version of Nginx
 NGINX_VHOSTSSL='y'            # enable centmin.sh menu 2 prompt to create self signed SSL vhost 2nd vhost conf
 NGINXBACKUP='y'
 NGINX_STAPLE_CACHE_OVERRIDE='n' # Enable will override Nginx OCSP stapling cache refresh time of 3600 seconds
