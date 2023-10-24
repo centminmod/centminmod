545176e71b971c740ffa807afbedf4d40d11245f
George Liu (eva2000) <eva2000@centminmod.com>
2023-10-25 02:14:51 +1000

    update Centmin Mod installers in 130.00beta01

--- installer82-gitlab.sh
+++ installer82-gitlab.sh
@@ -1849,7 +1849,7 @@ cd $INSTALLDIR
 # switch from PHP 5.4.41 to 5.6.9 default with Zend Opcache
 PHPVERLATEST=$(curl -${ipv_forceopt}sL https://www.php.net/downloads.php| egrep -o "php\-[0-9.]+\.tar[.a-z]*" | grep -v '.asc' | awk -F "php-" '/.tar.gz$/ {print $2}' | sed -e 's|.tar.gz||g' | uniq | grep '8.2' | head -n1)
 sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='$PHPVERLATEST'|" centmin.sh
-PHPVERLATEST=${PHPVERLATEST:-"8.2.7"}
+PHPVERLATEST=${PHPVERLATEST:-"8.2.11"}
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
 
 # disable axivo yum repo
