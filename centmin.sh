6d94b2f31f34b4512fe91c4555f86c26361777db
George Liu (eva2000) <eva2000@centminmod.com>
2024-05-30 02:48:13 +1000

    update nginx 1.27.0 default in 130.00beta01
    - security fixes for Nginx HTTP/3 though Centmin Mod by default doesn't enable Nginx HTTP/3 https://nginx.org/en/CHANGES
    - also update freenginx fork nginx 1.27.0 doesn't have HTTP/3 security fixes though not same as official nginx 1.27.0

--- centmin.sh
+++ centmin.sh
@@ -30,7 +30,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='130.00beta01'
 SCRIPT_MAJORVER='130'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='617'
+SCRIPT_INCREMENTVER='618'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='24/02/24'
@@ -1114,9 +1114,9 @@ MYSQL_INSTALL='n'            # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL='n'         # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.25.5'             # Use this version of Nginx
+NGINX_VERSION='1.27.0'             # Use this version of Nginx
 NGINX_ANGIE_VERSION='Angie-1.5.0'
-FREENGINX_VERSION='1.25.5'   # Maxim's Freenginx fork https://freenginx.org/en/download.html
+FREENGINX_VERSION='1.27.0'   # Maxim's Freenginx fork https://freenginx.org/en/download.html
 FREENGINX_INSTALL='n'        # Use Freenginx fork instead of official Nginx
 NGINX_VHOSTSSL='y'            # enable centmin.sh menu 2 prompt to create self signed SSL vhost 2nd vhost conf
 NGINXBACKUP='y'
