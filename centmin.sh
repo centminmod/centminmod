3ef8f4160d0156d20ba00e914b1e98e94a4834e0
George Liu (eva2000) <eva2000@centminmod.com>
2024-06-21 08:58:48 +1000

    update AWS-LC version v1.30.0 in 130.00beta01

--- centmin.sh
+++ centmin.sh
@@ -30,7 +30,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='130.00beta01'
 SCRIPT_MAJORVER='130'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='648'
+SCRIPT_INCREMENTVER='649'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='24/02/24'
@@ -1224,7 +1224,7 @@ BORINGSSL_DIR="/opt"
 
 # AWS-LC
 AWS_LC_SWITCH='n'             # if set to 'y' overrides OpenSSL as default for Nginx https://github.com/aws/aws-lc
-AWS_LC_VERSION='v1.29.0'      # version as per ttps://github.com/aws/aws-lc/tags
+AWS_LC_VERSION='v1.30.0'      # version as per ttps://github.com/aws/aws-lc/tags
 AWS_LC_DIR="/opt"
 AWS_LC_SWITCH_BUILD_TESTS='n' # run AWS-LC build tests
 ##################################
