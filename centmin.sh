d601296e74cfbb237322521d3417146106368dd0
George Liu (eva2000) <eva2000@centminmod.com>
2024-06-29 02:22:12 +1000

    update optional Angie Nginx fork 1.6.0 in 130.00beta01
    - update Angie Nginx fork version to 1.6.0 https://github.com/webserver-llc/angie/releases/tag/Angie-1.6.0

--- centmin.sh
+++ centmin.sh
@@ -30,7 +30,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='130.00beta01'
 SCRIPT_MAJORVER='130'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='677'
+SCRIPT_INCREMENTVER='678'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='24/02/24'
@@ -1165,7 +1165,7 @@ SENDMAIL_INSTALL='n'         # Install Sendmail (and mailx) set to y and POSTFIX
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
 NGINX_VERSION='1.27.0'             # Use this version of Nginx
-NGINX_ANGIE_VERSION='Angie-1.5.2'
+NGINX_ANGIE_VERSION='Angie-1.6.0'
 FREENGINX_VERSION='1.27.1'   # Maxim's Freenginx fork https://freenginx.org/en/download.html
 FREENGINX_INSTALL='n'        # Use Freenginx fork instead of official Nginx
 NGINX_VHOSTSSL='y'            # enable centmin.sh menu 2 prompt to create self signed SSL vhost 2nd vhost conf
