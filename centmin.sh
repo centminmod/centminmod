2379c62e4a983e8e8c9c9755e1f94b64a58cf07b
George Liu (eva2000) <eva2000@centminmod.com>
2024-10-04 10:56:36 +1000

    update nginx 1.27.2 & AWS-LC 1.36.1 in 140.00beta01
    - update nginx 1.27.2 default https://community.centminmod.com/threads/nginx-announce-nginx-1-27-2.26489/
    - update optional Nginx crypto library, AWS-LC 1.36.1 https://github.com/aws/aws-lc/releases/tag/v1.36.1
    
    nginx -V
    nginx version: nginx/1.27.2 (041024-004137-almalinux9-kvm-dda43d6)
    built by gcc 13.3.1 20240611 (Red Hat 13.3.1-2) (GCC)
    built with OpenSSL 1.1.1 (compatible; AWS-LC 1.36.1) (running with AWS-LC 1.36.1)

--- centmin.sh
+++ centmin.sh
@@ -30,7 +30,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='140.00beta01'
 SCRIPT_MAJORVER='140'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='138'
+SCRIPT_INCREMENTVER='139'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='01/07/24'
@@ -1205,7 +1205,7 @@ MYSQL_INSTALL='n'            # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL='n'         # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.27.1'             # Use this version of Nginx
+NGINX_VERSION='1.27.2'             # Use this version of Nginx
 NGINX_ANGIE_VERSION='Angie-1.7.0'
 FREENGINX_VERSION='1.27.4'     # Maxim's Freenginx fork https://freenginx.org/en/download.html
 FREENGINX_INSTALL='n'          # Use Freenginx fork instead of official Nginx
@@ -1268,7 +1268,7 @@ BORINGSSL_DIR="/opt"
 
 # AWS-LC
 AWS_LC_SWITCH='n'             # if set to 'y' overrides OpenSSL as default for Nginx https://github.com/aws/aws-lc
-AWS_LC_VERSION='v1.36.0'      # version as per ttps://github.com/aws/aws-lc/tags
+AWS_LC_VERSION='v1.36.1'      # version as per ttps://github.com/aws/aws-lc/tags
 AWS_LC_DIR="/opt"
 AWS_LC_SWITCH_BUILD_TESTS='n' # run AWS-LC build tests
 ##################################
