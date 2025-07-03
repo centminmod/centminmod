5617fcef4ae1eac7f601ff8627fb6428262c528e
George Liu <eva2000@centminmod.com>
2025-07-03 16:41:54 +1000

    update for Letsencrypt OCSP support EOL in 131.00stable
    - update Nginx HTTPS vhosts to disable OCSP stapling support as Letsencrypt OCSP support is EOL https://letsencrypt.org/2024/12/05/ending-ocsp/

--- addons/acmetool.sh
+++ addons/acmetool.sh
@@ -14,7 +14,7 @@ ARCH_CHECK="$(uname -m)"
 ###############################################################
 # variables
 ###############################################################
-ACMEVER='1.0.95'
+ACMEVER='1.0.96'
 DT=$(date +"%d%m%y-%H%M%S")
 ACMEDEBUG='n'
 ACMEDEBUG_LOG='y'
@@ -1416,8 +1416,8 @@ server {
   # enable ocsp stapling
   #resolver 8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1 valid=10m;
   #resolver_timeout 10s;
-  #ssl_stapling on;
-  #ssl_stapling_verify on;
+  #ssl_stapling off;
+  #ssl_stapling_verify off;
 
 # ngx_pagespeed & ngx_pagespeed handler
 #include /usr/local/nginx/conf/pagespeed.conf;
@@ -1814,8 +1814,8 @@ server {
   # enable ocsp stapling
   #resolver 8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1 valid=10m;
   #resolver_timeout 10s;
-  #ssl_stapling on;
-  #ssl_stapling_verify on;
+  #ssl_stapling off;
+  #ssl_stapling_verify off;
 
 # ngx_pagespeed & ngx_pagespeed handler
 #include /usr/local/nginx/conf/pagespeed.conf;
@@ -1927,8 +1927,8 @@ server {
   # enable ocsp stapling
   #resolver 8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1 valid=10m;
   #resolver_timeout 10s;
-  #ssl_stapling on;
-  #ssl_stapling_verify on;
+  #ssl_stapling off;
+  #ssl_stapling_verify off;
 
 # ngx_pagespeed & ngx_pagespeed handler
 #include /usr/local/nginx/conf/pagespeed.conf;
