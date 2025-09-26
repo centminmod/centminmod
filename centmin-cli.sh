c47cfa0273762dc0eda801e4aefa5167dc8d6bf3
George Liu <eva2000@centminmod.com>
2025-09-26 20:11:51 +1000

    switch Centmin Mod local mirror in 132.00stable

--- centmin-cli.sh
+++ centmin-cli.sh
@@ -30,7 +30,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='132.00stable'
 SCRIPT_MAJORVER='132'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='106'
+SCRIPT_INCREMENTVER='107'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='15/02/25'
@@ -708,7 +708,7 @@ CMGIT='https://github.com/centminmod/centminmod.git'
 # https://centminmod.com/upgrade.html#persistent
 AUTO_GITUPDATE='n'
 #####################################################
-LOCALCENTMINMOD_MIRROR='https://centminmod.com'
+LOCALCENTMINMOD_MIRROR='https://parts.centminmod.com'
 LOCAL_GEOLITE_TWO_DOMAIN='https://mxmind.centminmod.com'
 #####################################################
 # Timestamp Install
