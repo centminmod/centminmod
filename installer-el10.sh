26e1e5ab5497d66af4631e8fea128a3f1f7399b6
George Liu <eva2000@centminmod.com>
2025-10-08 22:14:19 +1000

    part 2 update EL10 routine for EL7 in 141.00beta01
    - ensure EL10 routine can still run on CentOS 7 OS servers with PHP 7.4.33 default instead of 8.3.26

--- installer-el10.sh
+++ installer-el10.sh
@@ -2489,13 +2489,13 @@ else
   PHPVERLATEST=$(curl -${ipv_forceopt}sL https://www.php.net/downloads.php?source=Y| grep -E -o "php-[0-9.]+\.tar[.a-z]*" | grep -v '.asc' | awk -F "php-" '/.tar.gz$/ {print $2}' | sed -e 's|.tar.gz||g' | uniq | grep '8.3' | head -n1)
 fi
 if [[ "$CENTOS_TEN" -eq '10' ]]; then
-  PHPVERLATEST=${PHPVERLATEST:-"8.3.21"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.3.26"}
 elif [[ "$CENTOS_NINE" -eq '9' ]]; then
-  PHPVERLATEST=${PHPVERLATEST:-"8.3.21"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.3.26"}
 elif [[ "$CENTOS_EIGHT" -eq '8' ]]; then
-  PHPVERLATEST=${PHPVERLATEST:-"8.3.21"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.3.26"}
 else
-  PHPVERLATEST=${PHPVERLATEST:-"8.3.21"}
+  PHPVERLATEST=${PHPVERLATEST:-"7.4.33"}
 fi
 sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='$PHPVERLATEST'|" centmin.sh
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
