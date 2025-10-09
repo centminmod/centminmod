aff728f253a70aa6429db41a35231ee3e0ff89ed
George Liu <eva2000@centminmod.com>
2025-10-09 10:27:16 +1000

    update Nginx 1.29.2 default in 132.00stable

--- centmin.sh
+++ centmin.sh
@@ -30,7 +30,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='132.00stable'
 SCRIPT_MAJORVER='132'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='109'
+SCRIPT_INCREMENTVER='110'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='15/02/25'
@@ -1169,9 +1169,9 @@ MYSQL_INSTALL='n'            # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL='n'         # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.29.1'             # Use this version of Nginx
+NGINX_VERSION='1.29.2'             # Use this version of Nginx
 NGINX_ANGIE_VERSION='Angie-1.6.0'
-FREENGINX_VERSION='1.27.3'   # Maxim's Freenginx fork https://freenginx.org/en/download.html
+FREENGINX_VERSION='1.29.2'   # Maxim's Freenginx fork https://freenginx.org/en/download.html
 FREENGINX_INSTALL='n'        # Use Freenginx fork instead of official Nginx
 NGINX_VHOSTSSL='y'            # enable centmin.sh menu 2 prompt to create self signed SSL vhost 2nd vhost conf
 NGINXBACKUP='y'
