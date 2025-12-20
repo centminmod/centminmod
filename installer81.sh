949b746fb497adcb2f4921b7a3c0a488702ad714
George Liu <eva2000@centminmod.com>
2025-12-21 01:29:29 +1000

    backport PHP 8.3.29 security fixes to PHP 7.4/8.0 EOL in 141.00beta01

--- installer81.sh
+++ installer81.sh
@@ -2452,11 +2452,11 @@ else
   PHPVERLATEST=$(curl -${ipv_forceopt}sL https://www.php.net/downloads.php?source=Y| grep -E -o "php-[0-9.]+\.tar[.a-z]*" | grep -v '.asc' | awk -F "php-" '/.tar.gz$/ {print $2}' | sed -e 's|.tar.gz||g' | uniq | grep '8.1' | head -n1)
 fi
 if [[ "$CENTOS_NINE" -eq '9' ]]; then
-  PHPVERLATEST=${PHPVERLATEST:-"8.1.33"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.1.34"}
 elif [[ "$CENTOS_EIGHT" -eq '8' ]]; then
-  PHPVERLATEST=${PHPVERLATEST:-"8.1.33"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.1.34"}
 else
-  PHPVERLATEST=${PHPVERLATEST:-"8.1.33"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.1.34"}
 fi
 sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='$PHPVERLATEST'|" centmin.sh
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
--- installer84.sh
+++ installer84.sh
@@ -2452,11 +2452,11 @@ else
   PHPVERLATEST=$(curl -${ipv_forceopt}sL https://www.php.net/downloads.php?source=Y| grep -E -o "php-[0-9.]+\.tar[.a-z]*" | grep -v '.asc' | awk -F "php-" '/.tar.gz$/ {print $2}' | sed -e 's|.tar.gz||g' | uniq | grep '8.4' | head -n1)
 fi
 if [[ "$CENTOS_NINE" -eq '9' ]]; then
-  PHPVERLATEST=${PHPVERLATEST:-"8.4.11"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.4.16"}
 elif [[ "$CENTOS_EIGHT" -eq '8' ]]; then
-  PHPVERLATEST=${PHPVERLATEST:-"8.4.11"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.4.16"}
 else
-  PHPVERLATEST=${PHPVERLATEST:-"8.4.11"}
+  PHPVERLATEST=${PHPVERLATEST:-"8.4.16"}
 fi
 sed -i "s|^PHP_VERSION='.*'|PHP_VERSION='$PHPVERLATEST'|" centmin.sh
 sed -i "s|ZOPCACHEDFT='n'|ZOPCACHEDFT='y'|" centmin.sh
