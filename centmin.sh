1ff8acd37b4459fe42aff8bb53c119ee0b9bfffd
George Liu <eva2000@centminmod.com>
2026-02-05 05:59:43 +1000

    update nginx 1.29.5 default in 140.00beta01
    - nginx 1.29.5 default https://nginx.org/en/CHANGES

--- centmin.sh
+++ centmin.sh
@@ -30,7 +30,7 @@ DT=$(date +"%d%m%y-%H%M%S")
 branchname='140.00beta01'
 SCRIPT_MAJORVER='140'
 SCRIPT_MINORVER='00'
-SCRIPT_INCREMENTVER='284'
+SCRIPT_INCREMENTVER='285'
 SCRIPT_VERSIONSHORT="${branchname}"
 SCRIPT_VERSION="${SCRIPT_VERSIONSHORT}.b${SCRIPT_INCREMENTVER}"
 SCRIPT_DATE='16/01/25'
@@ -1221,7 +1221,7 @@ MYSQL_INSTALL='n'            # Install official Oracle MySQL Server (MariaDB alt
 SENDMAIL_INSTALL='n'         # Install Sendmail (and mailx) set to y and POSTFIX_INSTALL=n for sendmail
 POSTFIX_INSTALL=y            # Install Postfix (and mailx) set to n and SENDMAIL_INSTALL=y for sendmail
 # Nginx
-NGINX_VERSION='1.29.4'             # Use this version of Nginx
+NGINX_VERSION='1.29.5'             # Use this version of Nginx
 NGINX_ANGIE_VERSION='Angie-1.10.1'
 FREENGINX_VERSION='1.29.2'     # Maxim's Freenginx fork https://freenginx.org/en/download.html
 FREENGINX_INSTALL='n'          # Use Freenginx fork instead of official Nginx
