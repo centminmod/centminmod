d8d21d40dd75f544d2a50e03eb101fe3d4af0957
George Liu <eva2000@centminmod.com>
2026-03-09 08:58:13 +1000

    fix acmetool.sh IPv6 listener sed regex escape for DEDI_LISTEN_V6 in 141.00beta01

--- addons/acmetool.sh
+++ addons/acmetool.sh
@@ -1062,7 +1062,7 @@ echo "sed -i 's|^##x# HTTPS-DEFAULT|#x# HTTPS-DEFAULT|g' \"/usr/local/nginx/conf
 echo "sed -i \"s|#x# server {| server {|\" \"/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf\""
 echo "sed -i \"s|#x#   $DEDI_LISTEN|   $DEDI_LISTEN|\" \"/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf\""
 if [[ "$VPS_IPSIX_CHECK_DISABLE" != [yY] && "$IP_SYSTEM_VALIDATE_V6" -eq '0' ]]; then
-  echo "sed -i \"s|#x#   $DEDI_LISTEN_V6|   $DEDI_LISTEN_V6|\" \"/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf\""
+  echo "sed -i \"s|#x#   $DEDI_LISTEN_V6_ESCAPED|   $DEDI_LISTEN_V6|\" \"/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf\""
 fi
 echo "sed -i \"s|#x#   server_name ${vhostname} www.${vhostname};|   server_name ${vhostname} www.${vhostname};|\" \"/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf\""
 echo "sed -i \"s|#x#   return 302 https://${vhostname}\$request_uri;|   return 302 https://${vhostname}\$request_uri;|\" \"/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf\""
@@ -1074,7 +1074,7 @@ sed -i 's|^##x# HTTPS-DEFAULT|#x# HTTPS-DEFAULT|g' "/usr/local/nginx/conf/conf.d
 sed -i "s|#x# server {| server {|" "/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf"
 sed -i "s|#x#   $DEDI_LISTEN|   $DEDI_LISTEN|" "/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf"
 if [[ "$VPS_IPSIX_CHECK_DISABLE" != [yY] && "$IP_SYSTEM_VALIDATE_V6" -eq '0' ]]; then
-  sed -i "s|#x#   $DEDI_LISTEN_V6|   $DEDI_LISTEN_V6|" "/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf"
+  sed -i "s|#x#   $DEDI_LISTEN_V6_ESCAPED|   $DEDI_LISTEN_V6|" "/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf"
 fi
 sed -i "s|#x#   server_name ${vhostname} www.${vhostname};|   server_name ${vhostname} www.${vhostname};|" "/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf"
 sed -i "s|#x#   return 302 https:\/\/${vhostname}\$request_uri;|   return 302 https:\/\/${vhostname}\$request_uri;|" "/usr/local/nginx/conf/conf.d/${vhostname}.ssl.conf"
@@ -1237,13 +1237,16 @@ if [[ -z "$SECOND_IP" ]]; then
   fi
   if [[ "$VPS_IPSIX_CHECK_DISABLE" != [yY] && "$IP_SYSTEM_VALIDATE_V6" -eq '0' ]]; then
     DEDI_LISTEN_V6="listen   [::]:80;"
+    DEDI_LISTEN_V6_ESCAPED=$(echo "$DEDI_LISTEN_V6" | sed 's/\[/\\[/g; s/\]/\\]/g')
     echo "DEDI_LISTEN_V6=\"listen   [::]:80;\""
     DEDI_LISTEN_HTTPS_V6="listen   [::]:443 ssl http2;"
     echo "DEDI_LISTEN_HTTPS_V6=\"listen   [::]:443 ssl http2;\""
   elif [[ "$VPS_IPSIX_CHECK_DISABLE" != [yY] && "$IP_SYSTEM_VALIDATE_V6" -ne '0' ]]; then
     DEDI_LISTEN_V6=""
+    DEDI_LISTEN_V6_ESCAPED=""
   else
     DEDI_LISTEN_V6=""
+    DEDI_LISTEN_V6_ESCAPED=""
   fi
 elif [[ "$SECOND_IP" ]]; then
   DEDI_IP=$(echo $(echo ${SECOND_IP}:))
@@ -1674,13 +1677,16 @@ if [[ -z "$SECOND_IP" ]]; then
   fi
   if [[ "$VPS_IPSIX_CHECK_DISABLE" != [yY] && "$IP_SYSTEM_VALIDATE_V6" -eq '0' ]]; then
     DEDI_LISTEN_V6="listen   [::]:80;"
+    DEDI_LISTEN_V6_ESCAPED=$(echo "$DEDI_LISTEN_V6" | sed 's/\[/\\[/g; s/\]/\\]/g')
     echo "DEDI_LISTEN_V6=\"listen   [::]:80;\""
     DEDI_LISTEN_HTTPS_V6="listen   [::]:443 ssl http2;"
     echo "DEDI_LISTEN_HTTPS_V6=\"listen   [::]:443 ssl http2;\""
   elif [[ "$VPS_IPSIX_CHECK_DISABLE" != [yY] && "$IP_SYSTEM_VALIDATE_V6" -ne '0' ]]; then
     DEDI_LISTEN_V6=""
+    DEDI_LISTEN_V6_ESCAPED=""
   else
     DEDI_LISTEN_V6=""
+    DEDI_LISTEN_V6_ESCAPED=""
   fi
 elif [[ "$SECOND_IP" ]]; then
   DEDI_IP=$(echo $(echo ${SECOND_IP}:))
