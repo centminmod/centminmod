f5db115f318dafbee039c709b31256bc077eb140
George Liu (eva2000) <centminmod@users.noreply.github.com>
2026-09-10 06:40:09 +1000

    update nodejs LTS 20 to 22 for EL8/EL9 in 141.00beta01
    node 20 LTS is EOL since April 2026, node 22 LTS runs to April 2027

--- addons/nodejs.sh
+++ addons/nodejs.sh
@@ -17,11 +17,11 @@ ARCH_CHECK="$(uname -m)"
 ######################################################
 # switch to nodesource yum repo instead of source compile
 # specify version branch so set NODEJSVER to 16 max for EL7
-# 20 for EL8 and EL9
+# 22 for EL8 and EL9
 # EL10 uses native AppStream nodejs instead of nodesource repo
 NODEJSVER='16'
-NODEJSVER_EL8='20'
-NODEJSVER_EL9='20'
+NODEJSVER_EL8='22'
+NODEJSVER_EL9='22'
 # for EL6 only
 NODEJS_SOURCEINSTALL='y'
 NODEJS_REINSTALL='y'
